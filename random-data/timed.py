e07d28bfda327ddb9f202d8a0171c43992d67245
Brad <brad.solomon.1124@gmail.com>
2018-06-28 12:58:55 -0400

    Add code examples

---
+++ random-data/timed.py
@@ -0,0 +1,19 @@
+import random
+import timeit
+
+_sysrand = random.SystemRandom()
+
+def prng() -> None:
+    random.randint(0, 95)
+
+def csprng() -> None:
+    _sysrand.randint(0, 95)
+
+setup = 'import random; from __main__ import prng, csprng'
+
+if __name__ == '__main__':
+    print('Best of 3 trials with 1,000,000 loops per trial:')
+
+    for f in ('prng()', 'csprng()'):
+        best = min(timeit.repeat(f, setup=setup))
+        print('\t{:8s} {:0.2f} seconds total time.'.format(f, best))
