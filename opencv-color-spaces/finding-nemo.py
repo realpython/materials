a0bae60b3d2769ffa64b63d1ae81f1b9795ea118
Rebecca Stone <ysbecca@gmail.com>
2018-09-06 18:50:36 +0100

    materials for opencv color spaces article

---
+++ opencv-color-spaces/finding-nemo.py
@@ -0,0 +1,188 @@
+
+# coding: utf-8
+
+# # Finding Nemo: understanding OpenCV colour spaces and how to use them
+# 
+# 
+# 
+# Code link for RealPython article with minimal commentary.
+# 
+# Author: Rebecca Stone @ysbecca
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ## Colour spaces
+# To get a list of all the possible colour conversions:
+
+flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
+print(len(flags), "flags total:")
+
+print(flags[40])
+
+# Let's look at our fish image.
+
+nemo = cv2.imread('./images/nemo0.jpg')
+plt.imshow(nemo)
+plt.show()
+
+
+# OpenCV by default opens images in BGR color space. 
+nemo = cv2.cvtColor(nemo, cv2.COLOR_BGR2RGB)
+
+
+plt.imshow(nemo)
+plt.show()
+
+
+# Segmenting Nemo
+# Plotting the image on 3D plot
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib import colors
+
+r, g, b = cv2.split(nemo)
+
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d") 
+
+pixel_colors = nemo.reshape((np.shape(nemo)[0]*np.shape(nemo)[1], 3))
+norm = colors.Normalize(vmin=-1.,vmax=1.)
+norm.autoscale(pixel_colors)
+pixel_colors = norm(pixel_colors).tolist()
+
+axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
+axis.set_xlabel("Red")
+axis.set_ylabel("Green")
+axis.set_zlabel("Blue")
+plt.show()
+
+from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
+
+hsv_nemo = cv2.cvtColor(nemo, cv2.COLOR_RGB2HSV)
+
+h, s, v = cv2.split(hsv_nemo)
+
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d") 
+
+axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
+axis.set_xlabel("Hue")
+axis.set_ylabel("Saturation")
+axis.set_zlabel("Value")
+plt.show()
+
+light_orange = (1, 190, 200)
+dark_orange = (18, 255, 255)
+
+# Normalise to 0 - 1 range for viewing
+lo_square = np.full((10, 10, 3), light_orange, dtype=np.uint8)  / 255.0
+do_square = np.full((10, 10, 3), dark_orange, dtype=np.uint8) / 255.0
+
+plt.subplot(1, 2, 1)
+plt.imshow(hsv_to_rgb(do_square))
+plt.subplot(1, 2, 2)
+plt.imshow(hsv_to_rgb(lo_square))
+plt.show()
+
+# Segment Nemo using inRange() function.
+
+mask = cv2.inRange(hsv_nemo, light_orange, dark_orange)
+
+# Bitwise-AND mask and original image
+result = cv2.bitwise_and(nemo, nemo, mask=mask)
+
+# Convert back to RGB in order to plot using `matplotlib.pyplot`. 
+
+plt.subplot(1, 2, 1)
+plt.imshow(mask, cmap="gray")
+plt.subplot(1, 2, 2)
+plt.imshow(result)
+plt.show()
+
+
+
+
+light_white = (0, 0, 200)
+dark_white = (145, 60, 255)
+
+lw_square = np.full((10, 10, 3), light_white, dtype=np.uint8) / 255.0
+dw_square = np.full((10, 10, 3), dark_white, dtype=np.uint8) / 255.0
+
+plt.subplot(1, 2, 1)
+plt.imshow(hsv_to_rgb(lw_square))
+plt.subplot(1, 2, 2)
+plt.imshow(hsv_to_rgb(dw_square))
+plt.show()
+
+mask_white = cv2.inRange(hsv_nemo, light_white, dark_white)
+
+# Bitwise-AND mask and original image
+result_white = cv2.bitwise_and(nemo, nemo, mask=mask_white)
+
+plt.subplot(1, 2, 1)
+plt.imshow(mask_white, cmap="gray")
+plt.subplot(1, 2, 2)
+plt.imshow(result_white)
+plt.show()
+
+final_mask = mask + mask_white
+
+final_result = cv2.bitwise_and(nemo, nemo, mask=final_mask)
+
+plt.subplot(1, 2, 1)
+plt.imshow(final_mask, cmap="gray")
+plt.subplot(1, 2, 2)
+plt.imshow(final_result)
+plt.show()
+
+blur = cv2.GaussianBlur(final_result, (7, 7), 0)
+
+plt.imshow(blur)
+plt.show()
+
+# Generalising the segmentation
+
+path = "./images/nemo"
+
+nemos_friends = []
+for i in range(6):
+   friend = cv2.cvtColor(cv2.imread(path + str(i) + ".jpg"), cv2.COLOR_BGR2RGB)
+   nemos_friends.append(friend)
+
+
+def segment_fish(image):
+    ''' Attempts to segment the clown fish out of the provided image. '''
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    
+    light_orange = (1, 190, 200)
+    dark_orange = (18, 255, 255)
+
+    mask = cv2.inRange(hsv_image, light_orange, dark_orange)
+    
+    light_white = (0, 0, 200)
+    dark_white = (145, 60, 255)
+    
+    mask_white = cv2.inRange(hsv_image, light_white, dark_white)
+    
+    final_mask = mask + mask_white
+    result = cv2.bitwise_and(image, image, mask=final_mask)
+    
+    blur = cv2.GaussianBlur(result, (7, 7), 0)
+    return result
+
+
+
+results = [segment_fish(friend) for friend in nemos_friends]
+
+for i in range(1, 6):
+    plt.subplot(1, 2, 1)
+    plt.imshow(nemos_friends[i])
+    plt.subplot(1, 2, 2)
+    plt.imshow(results[i])
+    plt.show()
+
+
+
