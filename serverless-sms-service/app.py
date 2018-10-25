dfb721d44e6cb81c7ecd5be358be4062426542ba
Saurabh Sharma <me@saurabhsharma.io>
2018-10-25 21:55:34 +0530

    refactoring - best practices

--- serverless-sms-service/app.py
+++ serverless-sms-service/app.py
@@ -1,21 +1,11 @@
-# stdlib imports
-from os import environ as env
-
-# 3rd party imports
+# core imports
 from chalice import Chalice, Response
-from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 
-# Twilio Config
-ACCOUNT_SID = env.get('ACCOUNT_SID')
-AUTH_TOKEN = env.get('AUTH_TOKEN')
-FROM_NUMBER = env.get('FROM_NUMBER')
-TO_NUMBER = env.get('TO_NUMBER')
+# app level imports
+from chalicelib import sms
 
-app = Chalice(app_name='serverless-sms-service')
-
-# create a twilio client using account_sid and auth token
-tw_client = Client(ACCOUNT_SID, AUTH_TOKEN)
+app = Chalice(app_name='sms-shooter')
 
 
 @app.route('/')
@@ -28,15 +18,12 @@ def send_sms():
     request_body = app.current_request.json_body
     if request_body:
         try:
-            msg = tw_client.messages.create(
-                from_=FROM_NUMBER,
-                body=request_body['msg'],
-                to=TO_NUMBER)
-            if msg.sid:
+            resp = sms.send(request_body)
+            if resp:
                 return Response(status_code=201,
                                 headers={'Content-Type': 'application/json'},
                                 body={'status': 'success',
-                                      'data': msg.sid,
+                                      'data': resp.sid,
                                       'message': 'SMS successfully sent'})
             else:
                 return Response(status_code=200,
