1ea78b8bb71e685c476d6fd98e829b6ad0a42123
Jasmine Finer <jasminefiner@me.com>
2018-12-09 19:19:46 +0000

    1c. Add Twitter Bootstrap To Your App

--- rp-portfolio/personal_portfolio/settings.py
+++ rp-portfolio/personal_portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "personal_portfolio.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["personal_portfolio/templates/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
