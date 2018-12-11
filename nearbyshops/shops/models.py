83d3e0a14d74229914d79330de774467505fe11e
Techiediaries <22133314+techiediaries@users.noreply.github.com>
2018-12-11 14:02:53 +0000

    Update models.py

--- nearbyshops/shops/models.py
+++ nearbyshops/shops/models.py
@@ -1,16 +1,8 @@
-#from django.db import models
-from django.contrib.gis.db import models
-
-from django.contrib.gis.geos import  Point
-
 # Create your models here.
-
+from django.contrib.gis.db import models
 
 class Shop(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-
-    def save(self, **kwargs):
-        super(Shop, self).save(**kwargs)
