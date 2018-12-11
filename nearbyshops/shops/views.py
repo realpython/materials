bfd59c6bbb341ffc936e8709a4a51509cd95c408
Techiediaries <22133314+techiediaries@users.noreply.github.com>
2018-12-11 14:20:42 +0000

    Update views.py

--- nearbyshops/shops/views.py
+++ nearbyshops/shops/views.py
@@ -1,17 +1,19 @@
 from django.views import generic
-from django.contrib.gis.geos import fromstr
+from django.contrib.gis.geos import  Point
 from django.contrib.gis.db.models.functions import Distance
 from .models import Shop
 
-longitude  = -80.191788
+longitude = -80.191788
 latitude = 25.761681
 
 user_location = Point(longitude, latitude, srid=4326)
- 
+
+
 class Home(generic.ListView):
     model = Shop
     context_object_name = 'shops'
-    queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6] 
-    template_name = 'shops/index.html' 
+    queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6]
+    template_name = 'shops/index.html'
 
+    
 home = Home.as_view()
