b02d3514ce11ae203d34e9b2bfe15c24923df523
Techiediaries <22133314+techiediaries@users.noreply.github.com>
2018-12-11 13:42:40 +0000

    Update admin.py

--- nearbyshops/shops/admin.py
+++ nearbyshops/shops/admin.py
@@ -3,22 +3,14 @@
 # Register your models here.
 
 from django.contrib import admin
-from django.contrib.gis.admin import GeoModelAdmin, OSMGeoAdmin
-from django.contrib.gis.geos import fromstr
-from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.admin import OSMGeoAdmin
 
 from .models import Shop
-
-user_location = fromstr('POINT(-9.544372557268286 30.35495327369988)', srid=4326)
  
 @admin.register(Shop)
 class ShopAdmin(OSMGeoAdmin):
     list_display= ('name','location')
 
-    def dist(self, obj):
-        d = obj.location.distance(user_location)
-        return d
-    dist.short_description = 'Distance'
 
 
 
