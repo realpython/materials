13866a362830ccecac3b3ee2df98430fa272a532
Techiediaries <22133314+techiediaries@users.noreply.github.com>
2018-12-11 13:43:41 +0000

    Update admin.py

--- nearbyshops/shops/admin.py
+++ nearbyshops/shops/admin.py
@@ -1,10 +1,7 @@
-#from django.contrib import admin
-
 # Register your models here.
 
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-
 from .models import Shop
  
 @admin.register(Shop)
