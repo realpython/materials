97bd2db832f152ba89daa1497dda9b9301bf0de5
Techiediaries <techiediaries9@gmail.com>
2018-12-12 23:36:10 +0000

    reformat code

--- nearbyshops/shops/migrations/0002_auto_20181020_0450.py
+++ nearbyshops/shops/migrations/0002_auto_20181020_0450.py
@@ -23,7 +23,7 @@ def load_data(apps, schema_editor):
                     longitude = obj.get('lon', 0)
                     latitude = obj.get('lat', 0)
                     location = fromstr(f'POINT({longitude} {latitude})',
-                                        srid=4326)
+                               srid=4326)
                     Shop(name=name, location=location).save()
             except KeyError:
                 pass
