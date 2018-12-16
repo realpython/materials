0091ee5421f8107e8629f1f22687ff224850b889
Jasmine Finer <jasminefiner@me.com>
2018-12-16 14:36:11 +0000

    2b. Projects App: Models

--- rp-portfolio/projects/models.py
+++ rp-portfolio/projects/models.py
@@ -1,3 +1,8 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(path="/img")
