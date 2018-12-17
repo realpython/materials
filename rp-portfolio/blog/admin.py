86c5009b84ef207c6dd1873ec9dc25afbe725347
Jasmine Finer <jasminefiner@me.com>
2018-12-17 20:48:00 +0000

    3b. Blog App: Django Admin

--- rp-portfolio/blog/admin.py
+++ rp-portfolio/blog/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
+from blog.models import Post, Category
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
