8ae8693470e7126a233f5d49685ad65bce3cb537
Jasmine Finer <jasminefiner@me.com>
2018-12-18 09:33:51 +0000

    3c. Blog App: Views

---
+++ rp-portfolio/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.blog_index, name="blog_index"),
+    path("<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("<category>/", views.blog_category, name="blog_category"),
+]
--- rp-portfolio/personal_portfolio/urls.py
+++ rp-portfolio/personal_portfolio/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
 ]
