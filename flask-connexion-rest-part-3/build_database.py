90d6a51cfab6b70367ecb0d79bae32120c6b25bc
Doug Farrell <doug.farrell@gmail.com>
2018-12-25 15:26:15 -0500

    Initial checkin of the flask-connexion-rest-part-3 code for the article. This includes the start of creating the relationships

---
+++ flask-connexion-rest-part-3/build_database.py
@@ -0,0 +1,50 @@
+import os
+from config import db
+from models import Person, Note
+
+# Data to initialize database with
+PEOPLE = [
+    {
+        "fname": "Doug",
+        "lname": "Farrell",
+        "notes": [
+            "Cool, a mini-blogging application!",
+            "This could be useful",
+            "Well, sort of useful"
+        ]
+    },
+    {
+        "fname": "Kent",
+        "lname": "Brockman",
+        "notes": [
+            "I'm going to make really profound observations",
+            "Maybe they'll be more obvious than I thought"
+        ]
+    },
+    {
+        "fname": "Bunny",
+        "lname": "Easter",
+        "notes": [
+            "Has anyone seen my Easter eggs?",
+            "I'm really late delivering these!"
+        ]
+    },
+]
+
+# Delete database file if it exists currently
+if os.path.exists("people.db"):
+    os.remove("people.db")
+
+# Create the database
+db.create_all()
+
+# iterate over the PEOPLE structure and populate the database
+for person in PEOPLE:
+    p = Person(lname=person.get("lname"), fname=person.get("fname"))
+
+    # Add the notes for the person
+    for note in person.get("notes"):
+        p.notes.append(Note(note=note))
+    db.session.add(p)
+
+db.session.commit()
---
+++ flask-connexion-rest-part-3/config.py
@@ -0,0 +1,26 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Build the Sqlite ULR for SqlAlchemy
+sqlite_url = "sqlite:////" + os.path.join(basedir, "people.db")
+
+# Configure the SqlAlchemy part of the app instance
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Create the SqlAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
