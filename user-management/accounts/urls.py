a630386c05296bcdf1ff165930bfc79b1da16b32
Jasmine Finer <jasminefiner@me.com>
2019-04-22 18:49:06 +0100

    1e. Let Users Update Their Profile

--- user-management/accounts/urls.py
+++ user-management/accounts/urls.py
@@ -1,8 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from accounts import views
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/signup/", views.SignUp.as_view(), name="signup"),
+    path(
+        "accounts/profile/",
+        login_required(
+            TemplateView.as_view(template_name="registration/profile.html")
+        ),
+        name="profile",
+    ),
+    path(
+        "accounts/edit_profile",
+        views.EditProfile.as_view(),
+        name="edit_profile",
+    ),
 ]
--- user-management/accounts/views.py
+++ user-management/accounts/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class EditProfile(UpdateView):
+    model = User
+    fields = ["username", "first_name"]
+    success_url = reverse_lazy("profile")
+    template_name = "registration/edit_profile_form.html"
+
+    def get_object(self):
+        return self.request.user
