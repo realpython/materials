c41f8dfc8af744e64c165016be546f04e609b5c5
Jasmine Finer <jasminefiner@me.com>
2019-04-22 18:42:23 +0100

    1c. Create Templates

--- user-management/user_management/settings.py
+++ user-management/user_management/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
