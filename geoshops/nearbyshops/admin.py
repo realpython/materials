610a2bcdd42a0e7e91ab91861792e66bd84131b6
jaclynn <jackie@ewilsons.com>
2019-04-29 21:02:05 -0400

    fix cci error

--- geoshops/nearbyshops/admin.py
+++ geoshops/nearbyshops/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from .models import Shop
 
+
 @admin.register(Shop)
 class ShopAdmin(OSMGeoAdmin):
     list_display = ('name', 'location')
