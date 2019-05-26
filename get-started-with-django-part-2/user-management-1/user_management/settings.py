7705bb90ad6a7c788c1d38c13eed2dc9cbe1f69a
Jasmine Finer <jasminefiner@me.com>
2019-05-26 11:46:20 +0100

    1c. Create Templates

--- get-started-with-django-part-2/user-management-1/user_management/settings.py
+++ get-started-with-django-part-2/user-management-1/user_management/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "user_management.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["user_management/templates/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
