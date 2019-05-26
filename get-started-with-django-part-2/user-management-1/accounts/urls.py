52cc727efaa72da91982a08a9805393dd2c35729
Jasmine Finer <jasminefiner@me.com>
2019-05-26 11:57:56 +0100

    1d. Register New Users

--- get-started-with-django-part-2/user-management-1/accounts/urls.py
+++ get-started-with-django-part-2/user-management-1/accounts/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path, include
+from accounts import views
 
-urlpatterns = [path("accounts/", include("django.contrib.auth.urls"))]
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
+]
