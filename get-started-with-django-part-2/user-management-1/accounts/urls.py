91195bc115428a2825b193e7450527e071e58c89
Jasmine Finer <jasminefiner@me.com>
2019-05-26 18:05:09 +0100

    1e. User Profiles

--- get-started-with-django-part-2/user-management-1/accounts/urls.py
+++ get-started-with-django-part-2/user-management-1/accounts/urls.py
@@ -1,7 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.views.generic import TemplateView
 from accounts import views
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
+    path(
+        "accounts/profile/",
+        login_required(
+            TemplateView.as_view(template_name="registration/profile.html")
+        ),
+        name="profile",
+    ),
+    path(
+        "accounts/edit_profile/",
+        views.EditProfileView.as_view(),
+        name="edit_profile",
+    ),
 ]
