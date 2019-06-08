1a5401f87a190976b96ce686bc0fe44515a8c1cd
Jasmine Finer <jasminefiner@me.com>
2019-06-08 23:12:10 +0100

    3b. Add Google Authentication

--- get-started-with-django-part-2/user-management-3/user_management/urls.py
+++ get-started-with-django-part-2/user-management-3/user_management/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path(
+        "accounts/profile/",
+        login_required(
+            TemplateView.as_view(template_name="accounts/profile.html")
+        ),
+        name="profile",
+    ),
 ]
