a5aaa9e9d56e26c52baae6b2c35b7cb8490cdcaf
martin-martin <breuss.martin@gmail.com>
2019-07-13 10:11:33 +0930

    Fix: format code using black

--- web-scraping-bs4/job_search.py
+++ web-scraping-bs4/job_search.py
@@ -18,8 +18,8 @@ def scrape_jobs(location=None):
         URL = f"https://www.monster.com/jobs/search/?q=Software-Developer"
     page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(id='ResultsContainer')
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="ResultsContainer")
     return results
 
 
@@ -33,10 +33,11 @@ def filter_jobs_by_keyword(results, word):
     :return: None - just meant to print results
     :rtype: None
     """
-    filtered_jobs = results.find_all('h2',
-                                     string=lambda text: word in text.lower())
+    filtered_jobs = results.find_all(
+        "h2", string=lambda text: word in text.lower()
+    )
     for f_job in filtered_jobs:
-        link = f_job.find('a')['href']
+        link = f_job.find("a")["href"]
         print(f_job.text.strip())
         print(f"Apply here: {link}\n")
 
@@ -51,19 +52,19 @@ def print_all_jobs(results):
     :return: None - just meant to print results
     :rtype: None
     """
-    job_elems = results.find_all('section', class_='card-content')
+    job_elems = results.find_all("section", class_="card-content")
 
     for job_elem in job_elems:
         # keep in mind that each job_elem is another BeautifulSoup object!
-        title_elem = job_elem.find('h2', class_='title')
-        company_elem = job_elem.find('div', class_='company')
-        location_elem = job_elem.find('div', class_='location')
+        title_elem = job_elem.find("h2", class_="title")
+        company_elem = job_elem.find("div", class_="company")
+        location_elem = job_elem.find("div", class_="location")
         if None in (title_elem, company_elem, location_elem):
             continue
             # print(job_elem.prettify())  # to inspect the 'None' element
         print(title_elem.text.strip())
-        link_elem = title_elem.find('a')
-        print(link_elem['href'])
+        link_elem = title_elem.find("a")
+        print(link_elem["href"])
         print(company_elem.text.strip())
         print(location_elem.text.strip())
         print()
@@ -71,16 +72,15 @@ def print_all_jobs(results):
 
 # USE THE SCRIPT AS A COMMAND-LINE INTERFACE
 # ----------------------------------------------------------------------------
-my_parser = argparse.ArgumentParser(prog='jobs',
-                                    description='Find Developer Jobs')
-my_parser.add_argument('-location',
-                       metavar='location',
-                       type=str,
-                       help='The location of the job')
-my_parser.add_argument('-word',
-                       metavar='word',
-                       type=str,
-                       help='What keyword to filter by')
+my_parser = argparse.ArgumentParser(
+    prog="jobs", description="Find Developer Jobs"
+)
+my_parser.add_argument(
+    "-location", metavar="location", type=str, help="The location of the job"
+)
+my_parser.add_argument(
+    "-word", metavar="word", type=str, help="What keyword to filter by"
+)
 
 args = my_parser.parse_args()
 location, keyword = args.location, args.word
--- web-scraping-bs4/scrape_jobs.py
+++ web-scraping-bs4/scrape_jobs.py
@@ -2,27 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer\
-        &where=Australia'
+URL = "https://www.monster.com/jobs/search/?q=Software-Developer\
+        &where=Australia"
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
 
 # Look for Python jobs
-python_jobs = results.find_all('h2',
-                               string=lambda t: "python" in t.lower())
+python_jobs = results.find_all("h2", string=lambda t: "python" in t.lower())
 for p_job in python_jobs:
-    link = p_job.find('a')['href']
+    link = p_job.find("a")["href"]
     print(p_job.text.strip())
     print(f"Apply here: {link}\n")
 
 # Print out all available jobs from the scraped webpage
-job_elems = results.find_all('section', class_='card-content')
+job_elems = results.find_all("section", class_="card-content")
 for job_elem in job_elems:
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
+    title_elem = job_elem.find("h2", class_="title")
+    company_elem = job_elem.find("div", class_="company")
+    location_elem = job_elem.find("div", class_="location")
     if None in (title_elem, company_elem, location_elem):
         continue
     print(title_elem.text.strip())
