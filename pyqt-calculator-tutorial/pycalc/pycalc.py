488a5aefea9a984ad51e4a52b985d3055e9ceacf
Leodanis Pozo Ramos <lpozor78@gmail.com>
2019-07-24 22:13:31 -0400

    Completing the View (GUI)

--- pyqt-calculator-tutorial/pycalc/pycalc.py
+++ pyqt-calculator-tutorial/pycalc/pycalc.py
@@ -18,6 +18,11 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import Qt
 
 __version__ = '0.1'
 __author__ = 'Leodanis Pozo Ramos'
@@ -33,9 +38,72 @@ class PyCalcUi(QMainWindow):
         # Set some main window's properties
         self.setWindowTitle('PyCalc')
         self.setFixedSize(235, 235)
-        # Create and set the central widget
+        # Create and set the central widget, and the general layout
+        self.generalLayout = QVBoxLayout()
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+        # Create the display, and the buttons
+        self._createDisplay()
+        self._createButtons()
+
+    def _createDisplay(self):
+        """Create the display."""
+        # Create the display widget
+        self.display = QLineEdit()
+        # Set some display's properties
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        # Add the display to the general layout
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        """Create the buttons."""
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+        #     btn_text | position in the QGridLayout
+        buttons = {'7': (0, 0),
+                   '8': (0, 1),
+                   '9': (0, 2),
+                   '/': (0, 3),
+                   'C': (0, 4),
+                   '4': (1, 0),
+                   '5': (1, 1),
+                   '6': (1, 2),
+                   '*': (1, 3),
+                   '(': (1, 4),
+                   '1': (2, 0),
+                   '2': (2, 1),
+                   '3': (2, 2),
+                   '-': (2, 3),
+                   ')': (2, 4),
+                   '0': (3, 0),
+                   '00': (3, 1),
+                   '.': (3, 2),
+                   '+': (3, 3),
+                   '=': (3, 4),
+                   }
+        # Create the buttons
+        for btn_text, pos in buttons.items():
+            self.buttons[btn_text] = QPushButton(btn_text)
+            self.buttons[btn_text].setFixedSize(40, 40)
+            buttonsLayout.addWidget(self.buttons[btn_text], pos[0], pos[1])
+        # Add the buttons to the general layout
+        self.generalLayout.addLayout(buttonsLayout)
+
+    def setDisplayText(self, text):
+        """Set display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self):
+        """Get display's text."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText('')
 
 
 # Client code
