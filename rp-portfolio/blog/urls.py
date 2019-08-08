729b115e849b7a4710cac426f60d026b1d94d4a4
UchihaUdai <udayposiya@gmail.com>
2019-08-09 01:01:28 +0530

    Update urls.py
    Using this app_name value we can easily redirect URL link to different patterns in view.py.

--- rp-portfolio/blog/urls.py
+++ rp-portfolio/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#Template tagging
+app_name ='blog
+
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("<int:pk>/", views.blog_detail, name="blog_detail"),
