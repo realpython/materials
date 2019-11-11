fd0ddc009747880cf4b932a7e5efa48d43d0004a
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2019-11-11 17:02:45 +0100

    Fix file formatting

--- binary-search/download_imdb.py
+++ binary-search/download_imdb.py
@@ -16,13 +16,14 @@ import urllib.request
 def main():
     """Script entry point."""
 
-    print('Fetching data from IMDb...')
+    print("Fetching data from IMDb...")
 
-    with open('names.txt', 'w') as destination:
+    with open("names.txt", "w") as destination:
         destination.writelines(names())
 
-    with open('names.txt') as source, \
-         open('sorted_names.txt', 'w') as destination:
+    with open("names.txt") as source, open(
+        "sorted_names.txt", "w"
+    ) as destination:
         destination.writelines(sorted(source.readlines()))
 
     print('Created "names.txt" and "sorted_names.txt"')
@@ -30,20 +31,20 @@ def main():
 
 def names():
     """Return a generator of names with a trailing newline."""
-    url = 'https://datasets.imdbws.com/name.basics.tsv.gz'
+    url = "https://datasets.imdbws.com/name.basics.tsv.gz"
     with urllib.request.urlopen(url) as response:
-        with tempfile.NamedTemporaryFile(mode='w+b') as archive:
+        with tempfile.NamedTemporaryFile(mode="w+b") as archive:
             shutil.copyfileobj(response, archive)
             archive.seek(0)
-            with gzip.open(archive, mode='rt') as source:
+            with gzip.open(archive, mode="rt") as source:
                 next(source)  # Skip the header
                 for line in source:
-                    full_name = line.split('\t')[1]
-                    yield f'{full_name}\n'
+                    full_name = line.split("\t")[1]
+                    yield f"{full_name}\n"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Aborted')
+        print("Aborted")
