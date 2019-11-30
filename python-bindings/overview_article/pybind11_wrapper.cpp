9f949aeee10fa30f29194f2629f753c38f9dbe27
Jim Anderson <jima.coding@gmail.com>
2019-11-29 17:11:25 -0700

    Fixing preflight issue with caps on comments

--- python-bindings/overview_article/pybind11_wrapper.cpp
+++ python-bindings/overview_article/pybind11_wrapper.cpp
@@ -2,6 +2,6 @@
 #include <cppmult.hpp>
 
 PYBIND11_MODULE(pybind11_example, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+    m.doc() = "pybind11 example plugin"; // Optional module docstring
     m.def("cpp_function", &cppmult, "A function which multiplies two numbers");
 }
