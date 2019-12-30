0c983255329383d8e4e06c50b3a963f014302391
Jim Anderson <jima80525@gmail.com>
2019-12-30 07:54:29 -0700

    Code clean up from Loic

--- python-bindings/overview_article/cppmult.cpp
+++ python-bindings/overview_article/cppmult.cpp
@@ -4,8 +4,11 @@
 
 float cppmult(int int_param, float float_param) {
     float return_value = int_param * float_param;
-    printf("    In cppmul: int: %d float %.1f returning  %.1f\n", int_param,
-            float_param, return_value);
-    return int_param * float_param;
+    std::cout << std::setprecision(1) << std::fixed
+	          << "    In cppmul: int: " << int_param
+	          << " float " <<  float_param
+	          << " returning  " << return_value
+	          << std::endl;
+    return return_value;
 }
 
