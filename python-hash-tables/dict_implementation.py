5c09d31fa93719982791a3668dc8989f56b5a96b
Adam Goldschmidt <adamgold7@gmail.com>
2020-03-12 10:51:58 +0200

    Create dict_implementation.py

---
+++ python-hash-tables/dict_implementation.py
@@ -0,0 +1,28 @@
+DUMMY = -2
+def generate_probes(hash_: int, mask: int) -> Iterable[int]:
+  # STEP 1 - calculate index
+  index = hash_ & mask
+  yield index
+  perturb = hash_
+  while True:
+    # STEP 6: hash collision. compute the new hash
+    new_hash = (index * 5 + 1 + perturb)
+    yield new_hash & mask
+    perturb >>= 5
+
+def lookup(key: Any, hash_: int) -> Tuple[int, Any]:
+  mask = len(indices) - 1
+  freeslot = None
+  for index in generate_probes(hash_, mask):
+    entry_index = indices[index]
+    elem = entries[entry_index]
+    if elem is None:
+      # STEP 2 & 3 - empty element
+      return (index, None) if freeslot is None else (freeslot, DUMMY)
+    if elem == DUMMY:
+      # STEP 3 - dummy element
+      if freeslot is None:
+        freeslot = index
+    # STEP 4 & 5: compare identity and hashes
+    if elem.key is key or (elem.hash == hash_ and elem.key == key):
+      return (index, elem)
