05099137d4f7fc9106c339814521db10f7eb9e13
Leodanis Pozo Ramos <lpozor78@gmail.com>
2020-03-25 11:16:38 -0400

    TR update

--- python-eval-mathrepl/mathrepl.py
+++ python-eval-mathrepl/mathrepl.py
@@ -8,7 +8,8 @@ __version__ = "1.0"
 __author__ = "Leodanis Pozo Ramos"
 
 ALLOWED_NAMES = {
-    name: obj for name, obj in math.__dict__.items() if "__" not in name
+    name: obj for name, obj in math.__dict__.items()
+    if not name.startswith("__")
 }
 
 PS1 = "mr>>"
