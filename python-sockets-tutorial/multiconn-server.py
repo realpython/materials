373a79dd4d3ef1a4f36e0ff41e6ec217bef8029b
Paolo Villaflores <pbvillaflores@gmail.com>
2020-05-03 11:09:18 +1000

    Add support for arbitrary sending of messages within multiconn-server.py

--- python-sockets-tutorial/multiconn-server.py
+++ python-sockets-tutorial/multiconn-server.py
@@ -7,6 +7,11 @@ import types
 
 sel = selectors.DefaultSelector()
 
+pending_messages = []    # Capture pending messages: (fd,message) where fd is the socket fd, a numeric integer value. For example:
+                         # The rest of the code can add items here to send messages.
+                         # For example:
+                         #     pending_messages.append( (key.fd, message) )
+
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
@@ -33,7 +38,9 @@ def service_connection(key, mask):
             print("echoing", repr(data.outb), "to", data.addr)
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
-
+        elif len(pending_messages) and pending_messages[0][0] == key.fd:
+            data.outb = pending_messages[0][1]
+            pending_messages.pop(0)
 
 if len(sys.argv) != 3:
     print("usage:", sys.argv[0], "<host> <port>")
