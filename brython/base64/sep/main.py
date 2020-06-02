063b72deb564aa9f2e9b4e8ac0603caf9bf3a168
Andre <andre.burgaud@gmail.com>
2020-06-02 11:10:03 -0500

    Added base64 example
    Added console example
    
    Added npm_install
    
    Added initial chrome extension examples

---
+++ brython/base64/sep/main.py
@@ -0,0 +1,22 @@
+from browser import document, prompt, html
+import base64
+
+b64_map = {}
+
+def base64_compute(evt):
+    default = "Real Python"
+    data = prompt("Enter a string:", default)
+    data = data or default
+    b64data = base64.b64encode(data.encode()).decode()
+    b64_map[data] = b64data
+    display_map()
+
+def display_map():
+    table = html.TABLE(style={'border': '1 solid grey'})
+    table <= html.TR(html.TH("Text") + html.TH("Base64"))
+    table <= (html.TR(html.TD(key) + html.TD(b64_map[key])) for key in b64_map)
+    base64_display = document["b64-display"]
+    base64_display.clear()
+    base64_display <= table
+
+document["b64-btn"].bind("click", base64_compute)
---
+++ brython/chrome_extensions/hello_js/popup.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let helloButton = document.getElementById("hello-btn");
+
+helloButton.onclick = function (element) {
+  const defaultName = "Real JavaScript";
+  let msg = "";
+  let name = prompt("Enter your name:", defaultName);
+  if (!name) {
+    name = defaultName;
+  }
+  document.getElementById("hello").innerHTML = `Hello, ${name}!`;
+};
