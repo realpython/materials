1ff59f0a4e08be692096d80343b3b385e4381e8d
Brad Solomon <bsolomon@protonmail.com>
2020-06-14 19:33:47 -0400

    Add <link> to normalize.css

--- django-security-headers/myapp/views.py
+++ django-security-headers/myapp/views.py
@@ -6,7 +6,12 @@ def index(request):
         """\
 <!DOCTYPE html>
 <html lang="en-US">
-  <head><meta charset="utf-8"><title>My secure app</title></head>
+  <head>
+    <meta charset="utf-8"><title>My secure app</title>
+    <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"
+    >
+  </head>
   <body><p>Now this is some sweet HTML!</p></body>
 </html>"""
     )
