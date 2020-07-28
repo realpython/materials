c53bc00463f5460a312fc075fe4ba241f14d946e
Pedro Pregueiro <pedropregueiro@gmail.com>
2020-07-28 15:07:15 +0200

    Create giphy_trending.py

---
+++ consuming-apis-python/giphy_trending.py
@@ -0,0 +1,13 @@
+import requests
+
+# Replace the following with the API key generated above.
+api_key = "API_KEY"
+endpoint = "https://api.giphy.com/v1/gifs/trending"
+params = {"api_key": api_key, "limit": 3, "rating": "g"}
+
+response = requests.get(endpoint, params=params).json()
+for gif in response["data"]:
+    title = gif["title"]
+    trending_date = gif["trending_datetime"]
+    url = gif["url"]
+    print("%s | %s | %s" % (title, trending_date, url))
