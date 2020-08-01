a76371a708212dc16d101df698dc076cd016093a
Christopher Trudeau <ctrudeau@arsensa.com>
2020-08-01 12:29:55 -0400

    Update pyflakes script to work with this repo, bad copy-and-pasted code, bad

--- django-view-auth/Blog/pyflakes.sh
+++ django-view-auth/Blog/pyflakes.sh
@@ -2,4 +2,4 @@
 
 echo "============================================================"
 echo "== pyflakes =="
-pyflakes `./manage.py print_setting LOCAL_APPS` | grep -v migration
+pyflakes core | grep -v migration
