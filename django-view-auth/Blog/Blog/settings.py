c04fa4b68ac986a76284e429b1146c7f2ff6d761
Christopher Trudeau <ctrudeau@arsensa.com>
2020-08-01 13:15:06 -0400

    Linter fix

--- django-view-auth/Blog/Blog/settings.py
+++ django-view-auth/Blog/Blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "Blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates"),],
+        "DIRS": [os.path.join(BASE_DIR, "templates"), ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
