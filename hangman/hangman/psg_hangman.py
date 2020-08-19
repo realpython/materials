1408f49e736200951b9bd48cd1c7fbfae6bd7588
Jon Fincher <jfincher42@gmail.com>
2020-08-19 16:44:41 -0500

    Some refactoring

--- hangman/hangman/psg_hangman.py
+++ hangman/hangman/psg_hangman.py
@@ -1,46 +1,84 @@
+"""Hangman for PySimpleGUI"""
+
+# Imports
 import PySimpleGUI as sg
 from random import choice
 
-# Hangman globals
-word_list = ["foolish", "gambler", "truncate", "boor"]
-letters_guessed = set()
-guesses_remaining = 6
-current_word = ""
-displayed_word = ""
-separator = " "
 
-# Hangman helper functions
-def build_displayed_word(word):
-    display = []
-    for letter in word:
-        if letter in letters_guessed:
-            display.append(letter)
-        else:
-            display.append("*")
+# Hangman helper functions from Basic game
+def select_word() -> str:
+    """Selects a random word from a list of known words.
+
+    Returns:
+        str: A word to be guessed
+    """
+    word_list = ["spam", "eggs", "monty", "python", "llama"]
+    return choice(word_list)
+
+
+def build_displayed_word(
+    current_word: str, letters_guessed: set
+) -> str:
+    """Builds the word to show the user
+    with the corrent letters in place
+
+    Args:
+        current_word (str): What is the word?
+        letters_guessed (set): Set of letters already guesses
+
+    Returns:
+        str: A string with guessed letters in the correct locations
+    """
+
+    # What do we separate the letters with
+    separator = " "
+    display = [
+        letter if letter in letters_guessed else "*"
+        for letter in current_word
+    ]
     return separator.join(display)
 
 
-def game_over():
-    if guesses_remaining == 0:
+def game_over(
+    guesses_taken: int, current_word: str, letters_guessed: set
+) -> bool:
+    """Determines end of game condition
+
+    Args:
+        guesses_taken (int): How many guesses have we taken?
+        current_word (str): What is the word to be guessed?
+        letters_guessed (set): Set of letters already guessed
+
+    Returns:
+        bool: True if the game is over, False if not
+    """
+
+    # Has the player taken all the guesses?
+    if guesses_taken == 6:
         return True
 
+    # Are all the letters in the current word all guessed?
     if set(current_word) <= letters_guessed:
         return True
 
+    # Then the game is not over
     return False
 
 
-def get_longest_word():
-    longest_word = ""
-    for word in word_list:
-        if len(word) > len(longest_word):
-            longest_word = word
-    return build_displayed_word(longest_word)
+# Hangman functions for GUI version
 
 
 # PSG: Define the layout element which contains our letter buttons
 def letter_frame():
-    letter_groups = ["ABCD", "EFGH", "IJKL", "MNOP", "QRST", "UVWX", "YZ"]
+    letter_groups = [
+        "ABCD",
+        "EFGH",
+        "IJKL",
+        "MNOP",
+        "QRST",
+        "UVWX",
+        "YZ",
+    ]
     return [
         [
             sg.Button(
@@ -49,7 +87,7 @@ def letter_frame():
                 border_width=0,
                 button_color=(None, sg.theme_background_color()),
                 key=f"-letter-{letter}-",
-                enable_events=True, 
+                enable_events=True,
             )
             for letter in letter_group
         ]
@@ -57,98 +95,114 @@ def letter_frame():
     ]
 
 
-# Create the main window layout
-layout = [
-    [
-        # Where do we draw the hangman?
-        sg.Frame(
-            "Hangman",
-            [
+if __name__ == "__main__":
+
+    # Create the main window layout
+    layout = [
+        [
+            # Where do we draw the hangman?
+            sg.Frame(
+                "Hangman",
                 [
-                    sg.Graph(
-                        canvas_size=(200, 400),
-                        graph_bottom_left=(0, 0),
-                        graph_top_right=(200, 400),
-                    )
-                ]
-            ],
-            font="Any 20",
-        ),
-        # Where are the letters we can guess?
-        sg.Column(
-            [
+                    [
+                        sg.Graph(
+                            canvas_size=(200, 400),
+                            graph_bottom_left=(0, 0),
+                            graph_top_right=(200, 400),
+                        )
+                    ]
+                ],
+                font="Any 20",
+            ),
+            # Where are the letters we can guess?
+            sg.Column(
                 [
-                    sg.Frame("Letters", letter_frame(), font="Any 20",),
-                    sg.Sizer(),
+                    [
+                        sg.Frame(
+                            "Letters", letter_frame(), font="Any 20",
+                        ),
+                        sg.Sizer(),
+                    ]
                 ]
-            ]
-        ),
-    ],
-    # Where is the word we are guessing?
-    [
-        sg.Frame(
-            "",
-            [
+            ),
+        ],
+        # Where is the word we are guessing?
+        [
+            sg.Frame(
+                "",
                 [
-                    sg.Text(
-                        text=get_longest_word(),
-                        key="-DISPLAY-WORD-",
-                        font="Courier 20",
-                    )
-                ]
-            ],
-            element_justification="center",
-        )
-    ],
-    # A Restart and Undo button would be helpful
-    [
-        sg.Frame(
-            "",
-            [
+                    [
+                        sg.Text(
+                            text="* * * * * * * * * *",
+                            key="-DISPLAY-WORD-",
+                            font="Courier 20",
+                        )
+                    ]
+                ],
+                element_justification="center",
+            )
+        ],
+        # A Restart and Undo button would be helpful
+        [
+            sg.Frame(
+                "",
                 [
-                    sg.Sizer(h_pixels=90),
-                    sg.Button(
-                        button_text="Restart", key="-RESTART-", font="Any 20"
-                    ),
-                    sg.Sizer(h_pixels=60),
-                    sg.Button(button_text="Undo", key="-UNDO-", font="Any 20"),
-                    sg.Sizer(h_pixels=90),
-                ]
-            ],
-            font="Any 20",
-        ),
-    ],
-]
+                    [
+                        sg.Sizer(h_pixels=90),
+                        sg.Button(
+                            button_text="Restart",
+                            key="-RESTART-",
+                            font="Any 20",
+                        ),
+                        sg.Sizer(h_pixels=60),
+                        sg.Button(
+                            button_text="Undo",
+                            key="-UNDO-",
+                            font="Any 20",
+                        ),
+                        sg.Sizer(h_pixels=90),
+                    ]
+                ],
+                font="Any 20",
+            ),
+        ],
+    ]
 
-# Define the window
-window = sg.Window("Hangman", layout, finalize=True)
+    # Initial game setup
+    # How many guesses have they taken?
+    guesses_taken = 0
 
-# Setup the game
+    # Which letters have been guessed already
+    letters_guessed = set()
 
-current_word = choice(word_list)
-displayed_word = build_displayed_word(current_word)
+    # Select the word to be guessed and make a display version
+    current_word = select_word()
+    displayed_word = build_displayed_word(current_word, letters_guessed)
 
-# Undo buttom is disabled until we have a guess
-window["-UNDO-"].update(disabled=True)
+    # Define the window
+    window = sg.Window("Hangman", layout, finalize=True)
 
-while True:
-    # Display the built word
-    window["-DISPLAY-WORD-"].update(displayed_word)
+    # Undo buttom is disabled until we have a guess
+    window["-UNDO-"].update(disabled=True)
 
-    # Get a button press
-    event, values = window.read()
-    print(event, values)
-    if event in (sg.WIN_CLOSED, "Exit"):
-        break
-    elif event[:8] == "-letter-":
-        player_guess = event[8].lower()
-        if player_guess not in set(current_word):
-            guesses_remaining -= 1
-        letters_guessed.add(player_guess)
-        displayed_word = build_displayed_word(current_word)
-        window["-UNDO-"].update(disabled=False)
-        window[event].update(disabled=True)
+    while True:
+        # Display the built word
+        window["-DISPLAY-WORD-"].update(displayed_word)
 
+        # Get a button press
+        event, values = window.read()
+        print(event, values)
+        if event in (sg.WIN_CLOSED, "Exit"):
+            break
+        elif event[:8] == "-letter-":
+            player_guess = event[8].lower()
+            if player_guess not in set(current_word):
+                guesses_taken += 1
+            letters_guessed.add(player_guess)
+            displayed_word = build_displayed_word(
+                current_word, letters_guessed
+            )
+            window["-UNDO-"].update(disabled=False)
+            window[event].update(disabled=True)
 
-
-window.close()
+    window.close()
