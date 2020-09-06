c14203b97fd9521e31cd5f1b2ba83cd6d4ebfcfa
Brad Solomon <bsolomon@protonmail.com>
2020-09-06 08:53:02 -0400

    black

--- django-gunicorn-nginx/project/settings.py
+++ django-gunicorn-nginx/project/settings.py
@@ -23,10 +23,7 @@ except KeyError as e:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "supersecure.codes",
-    "www.supersecure.codes"
-]
+ALLOWED_HOSTS = ["supersecure.codes", "www.supersecure.codes"]
 
 
 # Application definition
