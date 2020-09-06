817ac5100c870ca23a01e31b0dbde5b0a1fa40b7
Brad Solomon <bsolomon@protonmail.com>
2020-09-06 20:54:03 +0000

    Build out Gunicorn config

--- django-gunicorn-nginx/config/gunicorn/dev.py
+++ django-gunicorn-nginx/config/gunicorn/dev.py
@@ -10,3 +10,11 @@ workers = 2
 bind = "0.0.0.0:8000"
 # Restart workers when code changes (development only!)
 reload = True
+# Write access and error info to /var/log
+accesslog = errorlog = "/var/log/gunicorn/dev.log"
+# Redirect stdout/stderr to log file
+capture_output = True
+# PID file so you can easily fetch process ID
+pidfile = "/var/run/gunicorn/dev.pid"
+# Daemonize the Gunicorn process (detach & enter background)
+daemon = True
--- django-gunicorn-nginx/config/gunicorn/prod.py
+++ django-gunicorn-nginx/config/gunicorn/prod.py
@@ -6,3 +6,12 @@ import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
 bind = "0.0.0.0:8000"
+# Write access and error info to /var/log
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
+# Redirect stdout/stderr to log file
+capture_output = True
+# PID file so you can easily fetch process ID
+pidfile = "/var/run/gunicorn/prod.pid"
+# Daemonize the Gunicorn process (detach & enter background)
+daemon = True
