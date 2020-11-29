790acec1357b066de8eda4129fa131fb8c266391
Pedro Pregueiro <pedropregueiro@gmail.com>
2020-11-29 15:18:48 +0100

    Update github.py

--- consuming-apis-python/github.py
+++ consuming-apis-python/github.py
@@ -1,54 +1,87 @@
 import requests
 
-# Replace the CLIENT_ID and CLIENT_SECRET below with your own.
-client_id = "CLIENT_ID"
-client_secret = "CLIENT_SECRET"
-redirect_uri = "https://httpbin.org/anything"
-oauth_base_url = "https://github.com/login/oauth"
-api_base_url = "https://api.github.com"
+# First, make sure to follow the steps to create your own Github application:
+# https://docs.github.com/en/free-pro-team@latest/developers/apps/creating-an-oauth-app
 
 
-def get_code():
+# REPLACE the following variables with your Client ID and Client Secret
+CLIENT_ID = "<REPLACE_WITH_CLIENT_ID>"
+CLIENT_SECRET = "<REPLACE_WITH_CLIENT_SECRET>"
+
+# REPLACE the following variable with what you added in the "Authorization callback URL" field
+REDIRECT_URI = "<REPLACE_WITH_REDIRECT_URI>"
+
+# In this method you'll ask the Github API for a URL to redirect the user for authentication
+def create_oauth_link():
     params = {
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
+        "client_id": CLIENT_ID,
+        "redirect_uri": REDIRECT_URI,
         "scope": "user",
         "response_type": "code",
     }
 
-    endpoint = oauth_base_url + "/authorize"
-    url = requests.get(endpoint, params=params).url
-    print("Go to this URL and follow the OAuth flow there: %s" % url)
-    code = input("code: ")
-    return code
+    # This endpoint is defined in the Github documentation:
+    # https://docs.github.com/en/free-pro-team@latest/developers/apps/authorizing-oauth-apps#1-request-a-users-github-identity
+    endpoint = "https://github.com/login/oauth/authorize"
 
+    response = requests.get(endpoint, params=params)
+    
+    # When you make the request above, Github will redirect you to their website to input your credentials. Since you're doing
+    # this programmatically, you need to get the `url` parameter and print it in the console instead.
+    url = response.url
 
-def get_token(code=None):
-    endpoint = oauth_base_url + "/access_token"
+    return url
+
+# In this method you'll exchange the code you got from the Github API with an access token
+def exchange_code_for_access_token(code=None):
     params = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "redirect_uri": redirect_uri,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "redirect_uri": REDIRECT_URI,
         "code": code,
     }
+
+    # Here you define the content type you're expecting to get. In this case – JSON.
     headers = {"Accept": "application/json"}
-    response = requests.post(endpoint, data=params, headers=headers).json()
+
+
+    # This endpoint is defined in the Github documentation:
+    # https://docs.github.com/en/free-pro-team@latest/developers/apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
+    endpoint = "https://github.com/login/oauth/access_token"
+
+    response = requests.post(endpoint, params=params, headers=headers).json()
     return response["access_token"]
 
-
-def get_user(token=None):
-    headers = {"Authorization": "token %s" % token}
-
+# Finally in this method you'll print the user information re. its name, username and number of private repositories.
+def print_user_info(access_token=None):
+    # Now you need to send the `access_token` in the headers when calling the API
+    headers = {"Authorization": f"token {access_token}"}
+    
+    # This endpoint is defined in the Github documentation:
+    # https://docs.github.com/en/free-pro-team@latest/rest/reference/users#get-the-authenticated-user
     endpoint = "https://api.github.com/user"
+    
     response = requests.get(endpoint, headers=headers).json()
-    return response
 
+    # The response will be a dictionary with multiple user-related field. You can try `print(response)` to see all of them.
+    name = response["name"]
+    username = response["login"]
+    private_repos_count = response["total_private_repos"]
+    print(
+        f"{name} ({username}) | number of private repositories: {private_repos_count}"
+    )
 
-custom_code = get_code()
-access_token = get_token(code=custom_code)
-user = get_user(token=access_token)
+# So, one last time, step by step:
+# 1. Create a link to redirect the user to for authentication:
+link = create_oauth_link()
+print(f"Follow the link to start the authentication with Github: {link}")
 
-name = user["name"]
-username = user["login"]
-private_repos_count = user["total_private_repos"]
-print("%s (%s) | private repos: %s" % (name, username, private_repos_count))
+# 2. Paste the code you got from Github after authenticating
+code = input("Github code: ")
+
+# 3. Exchange that code with an access token
+access_token = exchange_code_for_access_token(code)
+print(f"Exchanged code {code} with access token: {access_token}")
+
+# 4. Fetch user information
+print_user_info(access_token=access_token)
