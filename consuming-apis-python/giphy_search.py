99ceca46f7fc4a108c9e442b003d89356941fde6
Pedro Pregueiro <pedropregueiro@gmail.com>
2020-11-29 15:18:00 +0100

    Update giphy_search.py

--- consuming-apis-python/giphy_search.py
+++ consuming-apis-python/giphy_search.py
@@ -1,11 +1,21 @@
 import requests
 
-# Replace the following with the API key generated.
-api_key = "API_KEY"
-endpoint = "https://api.giphy.com/v1/gifs/search"
-search_term = "shrug"
-params = {"api_key": api_key, "limit": 1, "q": search_term, "rating": "g"}
+# REPLACE the following with the API key you generated.
+API_KEY = "REPLACE_DEMO_API_KEY"
 
+endpoint = "https://api.giphy.com/v1/gifs/search"
+
+# Define the search term you want to use.
+search_term = "shrug"
+
+# In the query parameters you can define:
+# 1. The number of GIFs to return using the `limit` parameter.
+# 2. The query term to search for using the `q` parameter.
+# 3. The accepted rating of the GIFs returned using the `rating` parameter: https://developers.giphy.com/docs/optional-settings#rating
+params = {"api_key": API_KEY, "limit": 3, "q": search_term, "rating": "g"}
+
+# The response will contain a list with all the GIFs that match your query.
+# For each of those, you want to get the `title` and `url` fields.
 response = requests.get(endpoint, params=params).json()
 for gif in response["data"]:
     title = gif["title"]
