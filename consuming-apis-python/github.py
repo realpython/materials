ea28c792edc3d2b27339a1e746eb7408aa32b6d3
Pedro Pregueiro <pedropregueiro@gmail.com>
2020-12-12 18:17:48 +0100

    Update github.py

--- consuming-apis-python/github.py
+++ consuming-apis-python/github.py
@@ -8,10 +8,13 @@ import requests
 CLIENT_ID = "<REPLACE_WITH_CLIENT_ID>"
 CLIENT_SECRET = "<REPLACE_WITH_CLIENT_SECRET>"
 
-# REPLACE the following variable with what you added in the "Authorization callback URL" field
+# REPLACE the following variable with what you added in the
+# "Authorization callback URL" field.
 REDIRECT_URI = "<REPLACE_WITH_REDIRECT_URI>"
 
-# In this method you'll ask the Github API for a URL to redirect the user for authentication
+
+# In this method you'll ask the Github API for a URL to redirect the user
+# for authentication.
 def create_oauth_link():
     params = {
         "client_id": CLIENT_ID,
@@ -25,14 +28,18 @@ def create_oauth_link():
     endpoint = "https://github.com/login/oauth/authorize"
 
     response = requests.get(endpoint, params=params)
-    
-    # When you make the request above, Github will redirect you to their website to input your credentials. Since you're doing
-    # this programmatically, you need to get the `url` parameter and print it in the console instead.
+
+    # When you make the request above, Github will redirect you to their
+    # website to input your credentials. Since you're doing this
+    # programmatically, you need to get the `url` parameter and print it in
+    # the console instead.
     url = response.url
 
     return url
 
-# In this method you'll exchange the code you got from the Github API with an access token
+
+# In this method you'll exchange the code you got from the Github API with
+# an access token.
 def exchange_code_for_access_token(code=None):
     params = {
         "client_id": CLIENT_ID,
@@ -41,9 +48,9 @@ def exchange_code_for_access_token(code=None):
         "code": code,
     }
 
-    # Here you define the content type you're expecting to get. In this case – JSON.
+    # Here you define the content type you're expecting to get.
+    # In this case – JSON.
     headers = {"Accept": "application/json"}
-
 
     # This endpoint is defined in the Github documentation:
     # https://docs.github.com/en/free-pro-team@latest/developers/apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
@@ -52,24 +59,30 @@ def exchange_code_for_access_token(code=None):
     response = requests.post(endpoint, params=params, headers=headers).json()
     return response["access_token"]
 
-# Finally in this method you'll print the user information re. its name, username and number of private repositories.
+
+# Finally in this method you'll print the user information re. its name,
+# username and number of private repositories.
 def print_user_info(access_token=None):
-    # Now you need to send the `access_token` in the headers when calling the API
+    # Now you need to send the `access_token` in the headers
+    # when calling the API.
     headers = {"Authorization": f"token {access_token}"}
-    
+
     # This endpoint is defined in the Github documentation:
     # https://docs.github.com/en/free-pro-team@latest/rest/reference/users#get-the-authenticated-user
     endpoint = "https://api.github.com/user"
-    
+
     response = requests.get(endpoint, headers=headers).json()
 
-    # The response will be a dictionary with multiple user-related field. You can try `print(response)` to see all of them.
+    # The response will be a dictionary with multiple user-related field.
+    # You can try `print(response)` to see all of them.
     name = response["name"]
     username = response["login"]
     private_repos_count = response["total_private_repos"]
     print(
-        f"{name} ({username}) | number of private repositories: {private_repos_count}"
+        f"{name} ({username}) | "
+        f"number of private repositories: {private_repos_count}"
     )
+
 
 # So, one last time, step by step:
 # 1. Create a link to redirect the user to for authentication:
