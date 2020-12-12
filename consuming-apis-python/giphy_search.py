fef8549a4dd4543dcb379b64b3066e043a0a52f2
Pedro Pregueiro <pedropregueiro@gmail.com>
2020-12-12 17:57:47 +0100

    Update giphy_search.py

--- consuming-apis-python/giphy_search.py
+++ consuming-apis-python/giphy_search.py
@@ -11,7 +11,8 @@ search_term = "shrug"
 # In the query parameters you can define:
 # 1. The number of GIFs to return using the `limit` parameter.
 # 2. The query term to search for using the `q` parameter.
-# 3. The accepted rating of the GIFs returned using the `rating` parameter: https://developers.giphy.com/docs/optional-settings#rating
+# 3. The accepted rating of the GIFs returned using the `rating` parameter
+# https://developers.giphy.com/docs/optional-settings#rating
 params = {"api_key": API_KEY, "limit": 3, "q": search_term, "rating": "g"}
 
 # The response will contain a list with all the GIFs that match your query.
