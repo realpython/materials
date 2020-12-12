703dc8b34a315fc308789ce7550f2b52adf09333
Pedro Pregueiro <pedropregueiro@gmail.com>
2020-12-12 17:57:05 +0100

    Update covid.py

--- consuming-apis-python/covid.py
+++ consuming-apis-python/covid.py
@@ -3,8 +3,9 @@ import requests
 # The datetime package is useful to manipulate dates.
 from datetime import date, timedelta
 
-# To fetch the latest total confirmed cases number you need to pass a 24h window to the API.
-# That's why you need to pass both yesterday's date and today's.
+# To fetch the latest total confirmed cases number you need to pass a 24h
+# window to the API. That's why you need to pass both yesterday's date
+# and today's.
 today = date.today()
 yesterday = today - timedelta(days=1)
 
@@ -17,8 +18,9 @@ params = {"from": str(yesterday), "to": str(today)}
 # The response will be a list of results per day, in your case only 1 result.
 response = requests.get(endpoint, params=params).json()
 
-# Finally, you need to traverse through the response and increment the `total_confirmed` variable
-# with the total number of confirmed cases available that day, which is in the field `Cases`.
+# Finally, you need to traverse through the response and increment the
+# `total_confirmed` variable with the total number of confirmed cases
+# available that day, which is in the field `Cases`.
 total_confirmed = 0
 for day in response:
     cases = day.get("Cases", 0)
