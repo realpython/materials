8fc2226785e23cd865f0b313e0a6eb330dcc83cf
Cameron MacLeod <cmacleod170@gmail.com>
2021-03-05 09:42:00 +0000

    Indentation fixes

--- embedded-python/simon_says.py
+++ embedded-python/simon_says.py
@@ -7,28 +7,28 @@ points = 0
 
 # While the micro:bit is on
 while True:
-  # Pick a random direction
-  direction = directions[randrange(3)]
-  display.show(direction)
-  # Sleep for a second (1000ms)
-  sleep(1000)
-
-  # Get the X-axis (left-right) tilt
-  acc_x = accelerometer.get_x()
-  # Determine direction
-  if acc_x < -200:
-    user_in = "L"
-  elif abs(acc_x) < 200:
-    user_in = "O"
-  elif acc_x > 200:
-    user_in = "R"
-
-  # Check win condition
-  if user_in == direction:
-    # User input correctly
-    points += 1
-  else:
-    display.scroll(points)
-    display.show(Image.SAD)
-    points = 0
+    # Pick a random direction
+    direction = directions[randrange(3)]
+    display.show(direction)
+    # Sleep for a second (1000ms)
     sleep(1000)
+
+    # Get the X-axis (left-right) tilt
+    acc_x = accelerometer.get_x()
+    # Determine direction
+    if acc_x < -200:
+        user_in = "L"
+    elif abs(acc_x) < 200:
+        user_in = "O"
+    elif acc_x > 200:
+        user_in = "R"
+
+        # Check win condition
+        if user_in == direction:
+            # User input correctly
+            points += 1
+        else:
+            display.scroll(points)
+            display.show(Image.SAD)
+            points = 0
+            sleep(1000)
