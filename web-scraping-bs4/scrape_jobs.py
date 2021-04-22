c90bfd0340a0fa30ea8769b5b10c7e1bfbf0215d
martin-martin <breuss.martin@gmail.com>
2021-04-22 16:11:51 +0200

    Update code to match tutorial

--- web-scraping-bs4/scrape_jobs.py
+++ web-scraping-bs4/scrape_jobs.py
@@ -13,21 +13,17 @@ print("PYTHON JOBS\n==============================\n")
 python_jobs = results.find_all(
     "h2", string=lambda text: "python" in text.lower()
 )
-python_job_elems = [h2_elem.parent.parent.parent for h2_elem in python_jobs]
-for p_job in python_job_elems:
-    title_elem = p_job.find("h2", class_="title")
-    link_url = p_job.find_all("a")[1]["href"]
-    print(title_elem.text.strip())
-    print(f"Apply here: {link_url}\n")
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
 
-# Print out all available jobs from the scraped webpage
-print("ALL JOBS\n==============================\n")
-job_elems = results.find_all("div", class_="card-content")
-for job_elem in job_elems:
-    title_elem = job_elem.find("h2", class_="title")
-    company_elem = job_elem.find("h3", class_="company")
-    location_elem = job_elem.find("p", class_="location")
-    print(title_elem.text.strip())
-    print(company_elem.text.strip())
-    print(location_elem.text.strip())
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    link_url = job_element.find_all("a")[1]["href"]
+    print(f"Apply here: {link_url}\n")
     print()
