d432f999d31f38ad3ecffb9559487b4c2965ad1f
Jon Fincher <jfincher42@gmail.com>
2021-10-07 15:58:44 -0500

    Linting some more

--- top-python-game-engines/pygame_zero_sample/pygame_zero/pygame_zero_basic.py
+++ top-python-game-engines/pygame_zero_sample/pygame_zero/pygame_zero_basic.py
@@ -18,8 +18,6 @@ import pgzrun
 from pgzero.rect import Rect
 import pgzero.game
 
-screen: pgzero.game.screen
-
 # Set the width and height of our output window, in pixels
 WIDTH = 800
 HEIGHT = 600
@@ -29,20 +27,20 @@ def draw():
     """Draw is called once per frame to render everything on the screen"""
 
     # Clear the screen first
-    screen.clear()
+    pgzero.game.screen.clear()
 
     # Set the background color to white
-    screen.fill("white")
+    pgzero.game.screen.fill("white")
 
     # Draw a blue circle with a radius of 50 in the center of the screen
-    screen.draw.filled_circle((WIDTH // 2, HEIGHT // 2), 50, "blue")
+    pgzero.game.screen.draw.filled_circle((WIDTH // 2, HEIGHT // 2), 50, "blue")
 
     # Draw a red outlined square in the top left corner of the screen
     red_square = Rect((50, 50), (100, 100))
-    screen.draw.rect(red_square, (200, 0, 0))
+    pgzero.game.screen.draw.rect(red_square, (200, 0, 0))
 
     # Draw an orange caption along the bottom in 60 point font
-    screen.draw.text(
+    pgzero.game.screen.draw.text(
         "Hello, World! From Pygame Zero!",
         (100, HEIGHT - 50),
         fontsize=60,
