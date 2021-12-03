d2e4ec8fe8480e5b51c5b872d8498ed6a70470a7
martin-martin <breuss.martin@gmail.com>
2021-12-03 11:45:21 +0100

    Add tutorial code steps

---
+++ weather-app/source_code_step_2/weather.py
@@ -0,0 +1,14 @@
+from configparser import ConfigParser
+
+
+def _get_api_key():
+    """Fetch the API key from your configuration file.
+
+    Expects a configuration file named "secrets.ini" with structure:
+
+        [openweather]
+        api_key=<YOUR-OPENWEATHER-API-KEY>
+    """
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
