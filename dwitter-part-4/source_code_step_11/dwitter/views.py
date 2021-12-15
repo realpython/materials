7a1b05ac1af65c2f43fdd1a99c078e25f14a01a0
martin-martin <breuss.martin@gmail.com>
2021-12-15 10:43:30 +0100

    Add code steps for part 4

---
+++ dwitter-part-4/source_code_step_11/dwitter/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect, render
+
+from .forms import DweetForm
+from .models import Profile
+
+
+def dashboard(request):
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+    return render(request, "dwitter/dashboard.html", {"form": form})
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
+    return render(request, "dwitter/profile.html", {"profile": profile})
