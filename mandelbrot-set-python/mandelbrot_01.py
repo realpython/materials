187c0102493a8d1829ef3defa1750dc13b5c5a7f
Dan Bader <mail@dbader.org>
2022-02-14 16:10:23 -0800

    Black formatting pass

--- mandelbrot-set-python/mandelbrot_01.py
+++ mandelbrot-set-python/mandelbrot_01.py
@@ -10,7 +10,7 @@ class MandelbrotSet:
     def __contains__(self, c: complex) -> bool:
         z = 0
         for _ in range(self.max_iterations):
-            z = z ** 2 + c
+            z = z**2 + c
             if abs(z) > 2:
                 return False
         return True
