f6ae55726484bed697940adfb83cf706437589f2
Philipp <filip.axani+github@gmail.com>
2022-05-04 20:05:05 +0200

    Add code for TR

---
+++ fastapi-url-shortener/source_code_final/shortener_app/crud.py
@@ -0,0 +1,49 @@
+from sqlalchemy.orm import Session
+
+from . import utils, models, schemas
+
+
+def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+
+
+def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
+    key = utils.generate_unique_key(db)
+    secret_key = f"{key}_{utils.create_random_key(length=8)}"
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+
+def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.secret_key == secret_key, models.URL.is_active)
+        .first()
+    )
+
+
+def update_db_clicks(db: Session, db_url: schemas.URL) -> models.URL:
+    db_url.clicks += 1
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+
+def deactivate_db_url_by_secret_key(
+    db: Session, secret_key: str
+) -> models.URL:
+    db_url = get_db_url_by_secret_key(db, secret_key)
+    if db_url:
+        db_url.is_active = False
+        db.commit()
+        db.refresh(db_url)
+    return db_url
