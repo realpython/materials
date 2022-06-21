b6e80a1b643da41b3271980fe8178eb95bf8e3bf
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2022-06-21 13:55:27 +0200

    Queue - Sample Code

---
+++ queue/message_brokers/rabbitmq/producer.py
@@ -0,0 +1,16 @@
+# producer.py
+
+import pika
+
+QUEUE_NAME = "mailbox"
+
+with pika.BlockingConnection() as connection:
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME)
+    while True:
+        message = input("Message: ")
+        channel.basic_publish(
+            exchange="",
+            routing_key=QUEUE_NAME,
+            body=message.encode("utf-8")
+        )
