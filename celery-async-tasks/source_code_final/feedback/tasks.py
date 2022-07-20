cc075f4d9c3df88762d7459505b49fc7f3bbc795
martin-martin <breuss.martin@gmail.com>
2022-07-20 15:17:58 +0200

    Apply TR suggestions and fix a typo
    Co-authored-by: Philipp <filip.axani+github@gmail.com>

--- celery-async-tasks/source_code_final/feedback/tasks.py
+++ celery-async-tasks/source_code_final/feedback/tasks.py
@@ -6,12 +6,12 @@ from django.core.mail import send_mail
 
 @shared_task()
 def send_feedback_email_task(email_address, message):
-    """Sends an email when the feedback form had been submitted."""
+    """Sends an email when the feedback form has been submitted."""
     sleep(20)  # Simulate expensive operation that freezes Django
     send_mail(
         "Your Feedback",
         f"\t{message}\n\nThank you!",
-        "support@yourdomain.com",
+        "support@example.com",
         [email_address],
         fail_silently=False,
     )
--- celery-async-tasks/source_code_initial/feedback/forms.py
+++ celery-async-tasks/source_code_initial/feedback/forms.py
@@ -11,12 +11,12 @@ class FeedbackForm(forms.Form):
     )
 
     def send_email(self):
-        """Sends an email when the feedback form had been submitted."""
+        """Sends an email when the feedback form has been submitted."""
         sleep(20)  # Simulate expensive operation that freezes Django
         send_mail(
             "Your Feedback",
             f"\t{self.cleaned_data['message']}\n\nThank you!",
-            "support@yourdomain.com",
+            "support@example.com",
             [self.cleaned_data["email"]],
             fail_silently=False,
         )
