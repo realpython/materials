e06e352c0c0dcbe0ce4d62fdfdec7614ef020a84
Leodanis Pozo Ramos <lpozor78@gmail.com>
2022-08-02 16:24:52 +0200

    Python `exec()` sample code

---
+++ python-exec/compiled_code.py
@@ -0,0 +1,18 @@
+# compiled_code.py
+
+string_code = """
+def sum_of_even_squares(numbers):
+    return sum(number**2 for number in numbers if number % 2 == 0)
+
+print(sum_of_even_squares(numbers))
+"""
+
+compiled_code = compile(string_code, "<string>", "exec")
+exec(compiled_code)
+
+numbers = [2, 3, 7, 4, 8]
+exec(compiled_code)
+
+
+numbers = [5, 3, 9, 6, 1]
+exec(compiled_code)
---
+++ python-exec/hello.py
@@ -0,0 +1,8 @@
+# hello.py
+
+print("Hello, Pythonista!")
+print("Welcome to Real Python!")
+
+
+def greet(name="World"):
+    print(f"Hello, {name}!")
