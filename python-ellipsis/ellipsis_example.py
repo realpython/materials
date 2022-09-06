8252a4c689fb436bc97d9211bfa05fe12af1dfb4
Philipp <filip.axani+github@gmail.com>
2022-09-06 14:31:35 +0200

    Update Materials after TR

---
+++ python-ellipsis/ellipsis_example.py
@@ -0,0 +1,3 @@
+def do_nothing(): ...
+
+do_nothing()
--- python-ellipsis/flask_stub_example/app.py
+++ python-ellipsis/flask_stub_example/app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 
+from custom_stats import count_visitor
+
 app = Flask(__name__)
 
 if app.debug:
 
     def count_visitor():
         ...
-
-else:
-    import count_visitor
 
 
 @app.route("/")
