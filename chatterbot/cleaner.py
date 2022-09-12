d218e477dacf93a34e526aea5d33317c1e3955d9
martin-martin <breuss.martin@gmail.com>
2022-09-12 13:50:42 +0200

    Remove intro line, improve docstring

--- chatterbot/cleaner.py
+++ chatterbot/cleaner.py
@@ -9,8 +9,25 @@ def clean_corpus(chat_export_file):
 
 
 def remove_chat_metadata(chat_export_file):
-    """Remove WhatsApp chat metadata (date, time, username, and whitespace)."""
-    date_time = r"(\d+\/\d+\/\d+,\s\d+:\d+)"  # "8/26/19, 17:47"
+    """Remove WhatsApp chat metadata.
+
+    WhatsApp chat exports come with a boilerplate first intro line,
+    as well as metadata about each message:
+
+     date    time    username  message
+    ---------------------------------------
+    8/26/22, 17:47 - Jane Doe: Message text
+
+    This function removes the first boilerplate line of a chat export
+    as well as all the metadata up to the text of each message.
+
+    Args:
+        chat_export_file (str): The name of the chat export file
+
+    Returns:
+        tuple: The text of all messages in the conversation
+    """
+    date_time = r"(\d+\/\d+\/\d+,\s\d+:\d+)"  # "8/26/22, 17:47"
     dash_whitespace = r"\s-\s"  # " - "
     username = r"([\w\s]+)"  # "Jane Doe"
     metadata_whitespace = r":\s"  # ": "
@@ -19,20 +36,13 @@ def remove_chat_metadata(chat_export_file):
     with open(chat_export_file, "r") as corpus_file:
         content = corpus_file.read()
         cleaned_corpus = re.sub(pattern, "", content)
-        return tuple(cleaned_corpus.split("\n"))
+        return tuple(cleaned_corpus.split("\n")[1:])
 
 
 def remove_whatsapp_boilerplate(message_collection):
     """Remove conversation-irrelevant text from chat export."""
-    whatsapp_intro_line = (
-        "Messages and calls are end-to-end encrypted. "
-        "No one outside of this chat, not even WhatsApp, "
-        "can read or listen to them. Tap to learn more."
-    )
     boilerplate = (
-        whatsapp_intro_line,
         "<Media omitted>",
         "<media omitted>",
     )
-    breakpoint()
     return tuple((msg for msg in message_collection if msg not in boilerplate))
