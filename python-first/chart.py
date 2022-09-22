b32f546fc070101c0e8a581df8810fb9a2b141c2
Ian <iansedano@gmail.com>
2022-09-22 09:36:47 +0200

    adding ratio chart

--- python-first/chart.py
+++ python-first/chart.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 from test_fixtures import build_list
 
-TIMEIT_TIMES = 100_000
-LIST_SIZE = 500
+TIMEIT_TIMES = 100  # Increase number for smoother lines
+LIST_SIZE = 10000
 POSITION_INCREMENT = 10
 
 looping_times = []
@@ -74,7 +74,29 @@ plot = ax.plot(positions, in_times, label="in")
 
 plt.xlim([0, LIST_SIZE])
 plt.xlabel("Position of element to be found")
-plt.ylim([0, max(max(looping_times), max(generator_times))])
+plt.ylim([0, max(max(looping_times), max(generator_times), max(in_times))])
+plt.ylabel(f"Time to complete {TIMEIT_TIMES:,} times")
+plt.legend()
+
+plt.show()
+
+# Ratio
+
+looping_ratio = [loop / loop for loop in looping_times]
+generator_ratio = [
+    gen / loop for gen, loop in zip(generator_times, looping_times)
+]
+in_ratio = [in_ / loop for in_, loop in zip(in_times, looping_times)]
+
+fig, ax = plt.subplots()
+
+plot = ax.plot(positions, looping_ratio, label="loop")
+plot = ax.plot(positions, generator_ratio, label="generator")
+plot = ax.plot(positions, in_ratio, label="in")
+
+plt.xlim([0, LIST_SIZE])
+plt.xlabel("Position of element to be found")
+plt.ylim([0, max(max(looping_ratio), max(generator_ratio), max(in_ratio))])
 plt.ylabel(f"Time to complete {TIMEIT_TIMES:,} times")
 plt.legend()
 
