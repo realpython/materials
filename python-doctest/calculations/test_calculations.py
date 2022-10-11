c4037325b93edb2ba5192b4b07479e75018c2a6f
Leodanis Pozo Ramos <lpozor78@gmail.com>
2022-10-11 21:07:04 +0200

    Fix double quotes

---
+++ python-doctest/calculations/test_calculations.py
@@ -0,0 +1,14 @@
+import doctest
+import unittest
+
+import calculations
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocFileSuite("tests_file.txt"))
+    tests.addTests(doctest.DocTestSuite(calculations))
+    return tests
+
+
+if __name__ == "__main__":
+    unittest.main()
