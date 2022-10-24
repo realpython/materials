00c7a13560a30de81d34d7a3310be45edc738716
Leodanis Pozo Ramos <lpozor78@gmail.com>
2022-10-24 15:43:45 +0200

    TR updates, first round

--- python-getter-setter/employee4.py
+++ python-getter-setter/employee4.py
@@ -20,3 +20,11 @@ class Employee:
         self.name = name
         self.birth_date = birth_date
         self.start_date = start_date
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value.upper()
