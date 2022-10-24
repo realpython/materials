484c33fa17d91b68d6b97939019f108fed4657fa
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2022-10-24 19:48:42 +0200

    Power BI: Initial commit

---
+++ power-bi-python/02_extract_email.py
@@ -0,0 +1,5 @@
+# 'dataset' holds the input data for this script
+dataset = dataset.assign(
+    full_name=dataset["customer"].str.extract(r"([^<]+)"),
+    email=dataset["customer"].str.extract(r"<([^>]+)>"),
+).drop(columns=["customer"])
