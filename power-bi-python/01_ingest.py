b2d3acaa996cf95cfbb45526bccfbf732f1d532e
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2022-11-04 12:19:40 +0100

    Update 01_ingest.py
    The updated line became too long for the CI linter

--- power-bi-python/01_ingest.py
+++ power-bi-python/01_ingest.py
@@ -2,7 +2,7 @@ import sqlite3
 
 import pandas as pd
 
-with sqlite3.connect(r"C:\<REPLACE_WITH_PATH_TO_DB>\car_sales.db") as connection:
+with sqlite3.connect(r"C:\<YOUR_PATH_TO_DB>\car_sales.db") as connection:
     df = pd.read_sql_query("SELECT * FROM sales", connection)
 
 cars = df[
