8efe9faf049ea15be93a8cc56feeb313f14c9711
martin-martin <breuss.martin@gmail.com>
2022-11-16 14:01:35 +0100

    Shorten variable name

--- openai-dalle/convert.py
+++ openai-dalle/convert.py
@@ -13,5 +13,5 @@ with open(DATA_DIR / JSON_FILE, "r") as file:
 
 for index, image_dict in enumerate(response["data"]):
     image_data = b64decode(image_dict["b64_json"])
-    with open(IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png", "wb") as png_file:
-        png_file.write(image_data)
+    with open(IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png", "wb") as png:
+        png.write(image_data)
