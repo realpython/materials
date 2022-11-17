1084a6b9ca0cb589f5dbd8d4771c534c87623dc0
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2022-11-17 17:55:22 +0100

    bpython: Make linters happy (materials)

--- bpython/adder-v1.py
+++ bpython/adder-v1.py
@@ -2,8 +2,9 @@ try:
     x = int(input("x = "))
     y = int(input("y = "))
     z = x / y
-except (ValueError, ZeroDivisionError) as ex:
+except (ValueError, ZeroDivisionError) as ex:  # noqa: F841
     import bpython
+
     bpython.embed(locals(), banner="Post-Mortem Debugging:")
 else:
     print(z)
