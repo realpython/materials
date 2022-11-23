c597d27a2d02022d0addbe228c2e8cebf1a204aa
Geir Arne Hjelle <geirarne@gmail.com>
2022-11-23 11:29:31 +0100

    Final QA edits of OpenAI DALL-E materials

--- openai-dalle/vary.py
+++ openai-dalle/vary.py
@@ -5,12 +5,12 @@ from pathlib import Path
 
 import openai
 
-SOURCE_FILE = "A com-1667994848.json"
 DATA_DIR = Path.cwd() / "responses"
+SOURCE_FILE = DATA_DIR / "An ec-1667994848.json"
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-with open(DATA_DIR / SOURCE_FILE, "r") as json_file:
+with open(SOURCE_FILE, mode="r", encoding="utf-8") as json_file:
     saved_response = json.load(json_file)
     image_data = b64decode(saved_response["data"][0]["b64_json"])
 
@@ -21,7 +21,7 @@ response = openai.Image.create_variation(
     response_format="b64_json",
 )
 
-new_file_name = f"vary-{SOURCE_FILE[:4]}-{response['created']}.json"
+new_file_name = f"vary-{SOURCE_FILE.stem[:5]}-{response['created']}.json"
 
 with open(DATA_DIR / new_file_name, mode="w", encoding="utf-8") as file:
     json.dump(response, file)
