143b339dc91fb151271177b59e64c7a27aefae2d
Geir Arne Hjelle <geirarne@gmail.com>
2022-11-23 13:16:36 +0100

    Final QA edits of OpenAI DALL-E materials (#336)
    * Final QA edits of OpenAI DALL-E materials
    
    * Split long statement

--- openai-dalle/edit.py
+++ openai-dalle/edit.py
@@ -14,8 +14,8 @@ DESTINATION_PATH.mkdir(parents=True, exist_ok=True)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Image.create_edit(
-    image=open(SOURCE_PATH / "computer.png", "rb"),
-    mask=open(SOURCE_PATH / "mask.png", "rb"),
+    image=open(SOURCE_PATH / "computer.png", mode="rb"),
+    mask=open(SOURCE_PATH / "mask.png", mode="rb"),
     prompt=PROMPT,
     n=1,
     size="256x256",
