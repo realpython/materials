792b7f062d6e4e3d5a2866d2ff6179b502907051
Ian <iansedano@gmail.com>
2022-11-25 17:27:30 +0100

    catching ValueError

--- subprocess/custom_exit.py
+++ subprocess/custom_exit.py
@@ -6,3 +6,6 @@ try:
     raise SystemExit(int(sys.argv[1]))
 except IndexError as e:
     raise SystemExit(0) from e
+except ValueError as e:
+    print("Argument must be an integer")
+    raise SystemExit() from e
