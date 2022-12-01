eaa50810855b0fe7f824eafe959a87f5cb7a1f97
Philipp <filip.axani+github@gmail.com>
2022-12-01 15:35:20 +0100

    Fix linter

--- build-a-rest-api-frontend/source_code_final/app.py
+++ build-a-rest-api-frontend/source_code_final/app.py
@@ -12,11 +12,14 @@ app.add_api("swagger.yml")
 def home():
     people = Person.query.all()
     return render_template("home.html", people=people)
-    
+
+
 @app.route("/<person_name>")
 def person(person_name):
     print(person_name, file=sys.stderr)
-    person_object = Person.query.order_by(Person.fname.ilike("person_name")).first()
+    person_object = Person.query.order_by(
+        Person.fname.ilike("person_name")
+    ).first()
     print(person_object.fname, file=sys.stderr)
     return render_template("person.html", person=person_object)
 
