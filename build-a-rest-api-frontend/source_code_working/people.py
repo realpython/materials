0e6cc4dcf6c8fbbb848b94be92f10a1f1a2d77e5
Philipp <filip.axani+github@gmail.com>
2023-01-03 14:11:32 +0100

    Work with the ID of a person

--- build-a-rest-api-frontend/source_code_working/people.py
+++ build-a-rest-api-frontend/source_code_working/people.py
@@ -10,46 +10,41 @@ def read_all():
 
 
 def create(person):
-    lname = person.get("lname")
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
-
-    if existing_person is None:
-        new_person = person_schema.load(person, session=db.session)
-        db.session.add(new_person)
-        db.session.commit()
-        return person_schema.dump(new_person), 201
-    else:
-        abort(406, f"Person with last name {lname} already exists")
+    new_person = person_schema.load(person, session=db.session)
+    db.session.add(new_person)
+    db.session.commit()
+    return person_schema.dump(new_person), 201
 
 
-def read_one(lname):
-    person = Person.query.filter(Person.lname == lname).one_or_none()
+def read_one(person_id):
+    person = Person.query.get(person_id)
 
     if person is not None:
         return person_schema.dump(person)
     else:
-        abort(404, f"Person with last name {lname} not found")
+        abort(404, f"Person with ID {person_id} not found")
 
 
-def update(lname, person):
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+def update(person_id, person):
+    existing_person = Person.query.get(person_id)
 
     if existing_person:
         update_person = person_schema.load(person, session=db.session)
         existing_person.fname = update_person.fname
+        existing_person.lname = update_person.lname
         db.session.merge(existing_person)
         db.session.commit()
         return person_schema.dump(existing_person), 201
     else:
-        abort(404, f"Person with last name {lname} not found")
+        abort(404, f"Person with ID {person_id} not found")
 
 
-def delete(lname):
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+def delete(person_id):
+    existing_person = Person.query.get(person_id)
 
     if existing_person:
         db.session.delete(existing_person)
         db.session.commit()
-        return make_response(f"{lname} successfully deleted", 200)
+        return make_response(f"{person_id} successfully deleted", 200)
     else:
-        abort(404, f"Person with last name {lname} not found")
+        abort(404, f"Person with ID {person_id} not found")
