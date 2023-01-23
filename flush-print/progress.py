63259faa111a6cb55733a8ad18d354f21ff152a2
martin-martin <breuss.martin@gmail.com>
2023-01-23 15:55:18 +0100

    blacken

--- flush-print/progress.py
+++ flush-print/progress.py
@@ -2,7 +2,7 @@ from time import sleep
 
 
 def progress(percent=0, width=30):
-    progress = (width * percent // 100)
+    progress = width * percent // 100
     bar = "#" * progress
     pad = " " * (width - progress)
     print(f"\r[{bar}{pad}] {percent:.0f}%", end="", flush=True)
