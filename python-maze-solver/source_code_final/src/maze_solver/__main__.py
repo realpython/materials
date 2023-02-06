530af59695f3905cec436270f04a5849c10ae24d
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2023-02-06 14:31:10 +0100

    Maze Solver SbSp: Materials

---
+++ python-maze-solver/source_code_final/src/maze_solver/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+import pathlib
+
+from maze_solver.graphs.solver import solve_all
+from maze_solver.models.maze import Maze
+from maze_solver.view.renderer import SVGRenderer
+
+
+def main() -> None:
+    renderer = SVGRenderer()
+    for solution in solve_all(maze := Maze.load(parse_path())):
+        renderer.render(maze, solution).preview()
+    else:
+        print("No solution found")
+
+
+def parse_path() -> pathlib.Path:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=pathlib.Path)
+    return parser.parse_args().path
+
+
+if __name__ == "__main__":
+    main()
