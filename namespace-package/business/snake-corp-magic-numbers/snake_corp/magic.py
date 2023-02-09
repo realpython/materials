9d5a404c13f4bd1c63a9aec2916b8b5f403d72a8
Ian <iansedano@gmail.com>
2023-02-09 09:58:09 +0100

    Corporation instead of corp

--- namespace-package/business/snake-corp-magic-numbers/snake_corp/magic.py
+++ namespace-package/business/snake-corp-magic-numbers/snake_corp/magic.py
@@ -2,5 +2,5 @@ import random
 
 
 def secret_number_generator():
-    # Insert special Snake Corp sauce here ...
+    # Insert special Snake Corporation sauce here ...
     return random.randint(0, 9999)
