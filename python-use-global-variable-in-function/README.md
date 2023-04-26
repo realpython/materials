2bf00ad99f44570fd65efceb10abc05005fd4d65
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-04-26 10:48:15 +0200

    Sample code for the article on global variables in functions

---
+++ python-use-global-variable-in-function/README.md
@@ -0,0 +1,3 @@
+# Use and Define Global Variables in Your Python Functions
+
+This folder provides the code examples for the article [Use and Define Global Variables in Your Python Functions](https://realpython.com/python-use-global-variable-in-function/).
---
+++ python-use-global-variable-in-function/account_class.py
@@ -0,0 +1,46 @@
+class Account:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Successful deposit: +${amount:,.2f}")
+
+    def withdraw(self, amount):
+        if self.balance - amount > 0:
+            self.balance -= amount
+            print(f"Successful withdrawal: -${amount:,.2f}")
+        else:
+            print("Not enough funds for this transaction")
+
+    def get_balance(self):
+        return self.balance
+
+
+def main():
+    account = Account()
+    while True:
+        operation = input(
+            "What would you like to do?\n"
+            " d. deposit\n"
+            " w. withdraw\n"
+            " b. balance\n"
+            " q. quit\n"
+            "> "
+        )
+        if operation in "dD":
+            amount = float(input("Enter the deposit amount: "))
+            account.deposit(amount)
+        elif operation in "wW":
+            amount = float(input("Enter the withdrawal amount: "))
+            account.withdraw(amount)
+        elif operation in "bB":
+            print(f"Current balance: ${account.get_balance():,.2f}")
+        elif operation in "qQ":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid operation. Please try again.")
+
+
+main()
