99377abf0ab0e8213fca459928693aae0f1d9c70
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-05-19 09:07:46 +0200

    Update timing.py
    Finx an indentation issue in the `timing.py` file

--- python-callable-instances/timing.py
+++ python-callable-instances/timing.py
@@ -16,22 +16,21 @@ class ExecutionTimer:
     def __init__(self, repetitions=1):
         self.repetitions = repetitions
 
+    def __call__(self, func):
+        def timer(*args, **kwargs):
+            result = None
+            total_time = 0
+            print(f"Running {func.__name__}() {self.repetitions} times")
+            for _ in range(self.repetitions):
+                start = time.time()
+                result = func(*args, **kwargs)
+                end = time.time()
+                total_time += end - start
+            average_time = total_time / self.repetitions
+            print(
+                f"{func.__name__}() takes "
+                f"{average_time * 1000:.4f} ms on average"
+            )
+            return result
 
-def __call__(self, func):
-    def timer(*args, **kwargs):
-        result = None
-        total_time = 0
-        print(f"Running {func.__name__}() {self.repetitions} times")
-        for _ in range(self.repetitions):
-            start = time.time()
-            result = func(*args, **kwargs)
-            end = time.time()
-            total_time += end - start
-        average_time = total_time / self.repetitions
-        print(
-            f"{func.__name__}() takes "
-            f"{average_time * 1000:.4f} ms on average"
-        )
-        return result
-
-    return timer
+        return timer
