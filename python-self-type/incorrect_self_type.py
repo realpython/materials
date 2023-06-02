36b153bd0fd2d4584f660ae916b8f35dc1bbaa30
hfhoffman1144 <hfh160130@utdallas.edu>
2023-06-02 17:56:48 -0500

    deleted incorrect_self_type.py

--- python-self-type/incorrect_self_type.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-class Queue:
-    def __init__(self):
-        self.items = []
-
-    def enqueue(self, item:Any) -> Queue:
-        self.items.append(item)
-        return self
