739226702fd1ee061d86ff5de8a3259b8d36b735
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-06-05 13:21:22 +0200

    TR updates, first round

--- python-flatten-list/flatten.py
+++ python-flatten-list/flatten.py
@@ -3,8 +3,11 @@ from itertools import chain
 from operator import add, concat, iconcat
 
 
-def flatten_comprehension(matrix):
-    return [item for row in matrix for item in row]
+def flatten_extend(matrix):
+    flat = []
+    for row in matrix:
+        flat.extend(row)
+    return flat
 
 
 def flatten_concatenation(matrix):
@@ -14,15 +17,16 @@ def flatten_concatenation(matrix):
     return flat
 
 
-def flatten_extend(matrix):
-    flat = []
-    for row in matrix:
-        flat.extend(row)
-    return flat
+def flatten_comprehension(matrix):
+    return [item for row in matrix for item in row]
 
 
-def flatten_itertools_chain(matrix):
+def flatten_chain(matrix):
     return list(chain.from_iterable(matrix))
+
+
+def flatten_reduce_lambda(matrix):
+    return list(reduce(lambda x, y: x + y, matrix, []))
 
 
 def flatten_reduce_add(matrix):
@@ -35,10 +39,6 @@ def flatten_reduce_concat(matrix):
 
 def flatten_reduce_iconcat(matrix):
     return reduce(iconcat, matrix, [])
-
-
-def flatten_reduce_lambda(matrix):
-    return list(reduce(lambda x, y: x + y, matrix, []))
 
 
 def flatten_sum(matrix):
--- python-flatten-list/performance.py
+++ python-flatten-list/performance.py
@@ -6,15 +6,15 @@ SIZE = 1000
 TO_MS = 1000
 NUM = 10
 FUNCTIONS = [
-    "flatten_comprehension",
-    "flatten_concatenation",
     "flatten_extend",
+    "flatten_concatenation",
+    "flatten_comprehension",
+    "flatten_chain",
     "flatten_reduce_lambda",
-    "flatten_sum",
-    "flatten_itertools_chain",
     "flatten_reduce_add",
     "flatten_reduce_concat",
     "flatten_reduce_iconcat",
+    "flatten_sum",
 ]
 
 matrix = [list(range(SIZE))] * SIZE
@@ -27,4 +27,4 @@ results = {
 print(f"Time to flatten a {SIZE}x{SIZE} matrix (in milliseconds):\n")
 
 for func, time in sorted(results.items(), key=lambda result: result[1]):
-    print(f"{func + '()':.<30}{time * TO_MS / NUM:.>10.6f} ms")
+    print(f"{func + '()':.<30}{time * TO_MS / NUM:.>7.2f} ms")
