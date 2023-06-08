61ab88574fa0ea69f63a40b1a63efb8c02396b35
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-06-08 12:19:26 +0200

    TR updates

--- python-flatten-list/flatten_numpy_flatten.py
+++ python-flatten-list/flatten_numpy_flatten.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-matrix = [list(range(100))] * 4
-arr = np.array(matrix)
+matrix = np.array(
+[
+    [9, 3, 8, 3],
+    [4, 5, 2, 8],
+    [6, 4, 3, 1],
+    [1, 0, 4, 5],
+])
 
 matrix.flatten()
