650c7eeb7988fded29d33a0f4dc6965b63878f7d
martin-martin <breuss.martin@gmail.com>
2023-06-21 17:37:31 +0200

    Fix a bracket

--- prompt-engineering/src/bleach/cli.py
+++ prompt-engineering/src/bleach/cli.py
@@ -13,7 +13,7 @@ def cli_prompt() -> argparse.Namespace:
     """Parse the arguments provided through the CLI."""
     parser = argparse.ArgumentParser(
         description="Sanitize chat support text with OpenAI's API"
-        )
+    )
     parser.add_argument("--file", help="Path to file with text")
 
     args = parser.parse_args()
