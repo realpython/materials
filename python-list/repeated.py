c72b22e1286f726d6fbe5e9d2e1f18acece49a37
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-06-28 13:24:18 +0200

    TR update, second round

--- python-list/repeated.py
+++ python-list/repeated.py
@@ -4,17 +4,3 @@ def get_unique_items(list_object):
         if item not in result:
             result.append(item)
     return result
-
-
-print(get_unique_items([2, 4, 5, 2, 3, 5]))
-
-
-def get_unique_items_tail(list_object):
-    result = []
-    for item in reversed(list_object):
-        if item not in result:
-            result.insert(0, item)
-    return result
-
-
-print(get_unique_items_tail([2, 4, 5, 2, 3, 5]))
