3b3b7e4cf719ee2a9f6259927a0029bfd3ff1226
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-07-25 12:40:41 +0100

    Update perform_operation2.py

--- working-with-the-python-operator-module/perform_operation2.py
+++ working-with-the-python-operator-module/perform_operation2.py
@@ -1,8 +1,7 @@
+from operator import add, sub, mul, truediv
+
 def perform_operation(operator_function, operand1, operand2):
     return operator_function(operand1, operand2)
-
-
-from operator import add, sub, mul, truediv
 
 number1 = 10
 number2 = 5
