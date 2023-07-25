f854c5b275c96d9fe0a38488b2b3ab0803114694
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-07-25 12:38:00 +0100

    Update attrgetter_min_max.py

--- working-with-the-python-operator-module/attrgetter_min_max.py
+++ working-with-the-python-operator-module/attrgetter_min_max.py
@@ -1,3 +1,4 @@
+import operator
 from dataclasses import dataclass
 
 
@@ -23,36 +24,6 @@ group_members = []
 
 for musician in musicians_list:
     group_members.append(Musician(*musician))
-
-import operator
-
-from dataclasses import dataclass
-
-
-@dataclass
-class Musician:
-    id: int
-    fname: str
-    lname: str
-    group: str
-
-
-musicians_list = [
-    [1, "Brian", "Wilson", "Beach Boys"],
-    [2, "Carl", "Wilson", "Beach Boys"],
-    [3, "Dennis", "Wilson", "Beach Boys"],
-    [4, "Bruce", "Johnston", "Beach Boys"],
-    [5, "Hank", "Marvin", "Shadows"],
-    [6, "Bruce", "Welch", "Shadows"],
-    [7, "Brian", "Bennett", "Shadows"],
-]
-
-group_members = []
-
-for musician in musicians_list:
-    group_members.append(Musician(*musician))
-
-import operator
 
 get_id = operator.attrgetter("id")
 
