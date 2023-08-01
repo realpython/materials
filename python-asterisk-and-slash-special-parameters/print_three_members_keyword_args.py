7ada0938996fcae66876b667390b3cfb4e43e98a
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-01 10:49:43 +0100

    Update print_three_members_keyword_args.py

--- python-asterisk-and-slash-special-parameters/print_three_members_keyword_args.py
+++ python-asterisk-and-slash-special-parameters/print_three_members_keyword_args.py
@@ -1,7 +1,8 @@
-def print_three_members(*, member1, member2, member3 ):
+def print_three_members(*, member1, member2, member3):
     print(f"member1 is {member1}")
     print(f"member2 is {member2}")
     print(f"member3 is {member3}")
+
 
 print_three_members(member1="Frank", member2="Dean", member3="Sam")
 
