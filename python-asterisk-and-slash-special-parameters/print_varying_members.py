41a0cbc3907132f8ff0e2f209075686ed3906300
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-01 12:37:10 +0100

    Update print_varying_members.py

--- python-asterisk-and-slash-special-parameters/print_varying_members.py
+++ python-asterisk-and-slash-special-parameters/print_varying_members.py
@@ -4,7 +4,8 @@ def print_varying_members(member1, member2, *args, member3):
     print(f"member3 is {member3}")
     print(f"*args contains {args}")
 
+
 print_varying_members("Frank", member2="Dean", member3="Sam")
 print_varying_members(member1="Frank", member2="Dean", member3="Sam")
-# print_varying_members(member1="Frank", "Dean", member3="Sam") 
+# print_varying_members(member1="Frank", "Dean", member3="Sam")
 print_varying_members("Frank", "Dean", "Peter", "Joey", member3="Sam")
