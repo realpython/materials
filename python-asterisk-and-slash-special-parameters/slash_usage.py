6bbbdae007f42a2318df31332995be03bccf7671
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-01 12:37:28 +0100

    Update slash_usage.py

--- python-asterisk-and-slash-special-parameters/slash_usage.py
+++ python-asterisk-and-slash-special-parameters/slash_usage.py
@@ -3,6 +3,7 @@ def slash_usage(position_only, /, keyword_only):
 
 slash_usage("Frank", keyword_only="Dean")
 
+
 slash_usage(position_only="Frank", keyword_only="Dean")
 
-# slash_usage(position_only="Frank", "Dean") 
+# slash_usage(position_only="Frank", "Dean")
