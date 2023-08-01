8850606156bcd2af9eea43fad8245b014dabffc2
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-01 10:45:52 +0100

    Update print_three_members_both_operators.py

--- python-asterisk-and-slash-special-parameters/print_three_members_both_operators.py
+++ python-asterisk-and-slash-special-parameters/print_three_members_both_operators.py
@@ -1,4 +1,4 @@
-def print_three_members(member1, member2, /, *, member3 ):
+def print_three_members(member1, member2, /, *, member3):
     print(f"member1 is {member1}")
     print(f"member2 is {member2}")
     print(f"member3 is {member3}")
