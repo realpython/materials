17ab72ecf2b26035cbec33e0dec1b28dee3a7cfa
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-01 10:50:29 +0100

    Update asterisk_usage.py

--- python-asterisk-and-slash-special-parameters/asterisk_usage.py
+++ python-asterisk-and-slash-special-parameters/asterisk_usage.py
@@ -1,6 +1,7 @@
 def asterisk_usage(either, *, keyword_only):
     print(either, keyword_only)
 
+
 asterisk_usage(either="Frank", keyword_only="Dean")
 
 asterisk_usage("Frank", keyword_only="Dean")
