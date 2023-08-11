812b891def85319bc9c159ad2c7a7935d5524a1a
gahjelle <geirarne@gmail.com>
2023-08-11 09:48:36 +0200

    Final QA

--- python-asterisk-and-slash-special-parameters/slash_usage.py
+++ python-asterisk-and-slash-special-parameters/slash_usage.py
@@ -1,9 +1,10 @@
-def slash_usage(position_only, /, keyword_only):
-    print(position_only, keyword_only)
+def slash_usage(position_only, /, either):
+    print(position_only, either)
 
 
-slash_usage("Frank", keyword_only="Dean")
+slash_usage("Frank", either="Dean")
+slash_usage("Frank", "Dean")
 
 # This would be invalid:
-# slash_usage(position_only="Frank", keyword_only="Dean")
+# slash_usage(position_only="Frank", either="Dean")
 # slash_usage(position_only="Frank", "Dean")
