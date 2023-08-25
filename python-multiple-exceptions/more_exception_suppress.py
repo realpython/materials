45892d8ea62266ad4d3c058d7b4539f291569ab8
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-25 20:15:29 +0100

    Update more_exception_suppress.py

--- python-multiple-exceptions/more_exception_suppress.py
+++ python-multiple-exceptions/more_exception_suppress.py
@@ -10,9 +10,10 @@ main_file = pathlib.Path("all_transactions.txt")
 while True:
     archive_path = temporary_file.with_suffix(f".arch_{time.time()}")
     with suppress(FileNotFoundError):
-        time.sleep(3)
         with temporary_file.open(mode="rt") as transactions:
             with main_file.open(mode="at") as main:
                 print("Found new transactions, updating log, & archiving")
                 main.writelines(transactions.readlines())
         temporary_file.replace(archive_path)
+    time.sleep(3)
+    
