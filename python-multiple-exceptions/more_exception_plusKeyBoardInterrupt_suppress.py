3ec4c29ff02db60e6390cc9173e1418d7dffd1e5
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-08-25 18:40:43 +0100

    Update more_exception_plusKeyBoardInterrupt_suppress.py

--- python-multiple-exceptions/more_exception_plusKeyBoardInterrupt_suppress.py
+++ python-multiple-exceptions/more_exception_plusKeyBoardInterrupt_suppress.py
@@ -17,3 +17,4 @@ with suppress(KeyboardInterrupt):
                     print("Found new transactions, updating log, & archiving")
                     main.writelines(transactions.readlines())
             temporary_file.replace(archive_path)
+            
