52947ca1080bd6d35e1548d984a5c2187ed318a3
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2023-08-28 17:47:04 +0200

    Bypassing the GIL: Apply more TR feedback

--- python-parallel-processing/07_image_processing/image_processing_bonus.py
+++ python-parallel-processing/07_image_processing/image_processing_bonus.py
@@ -1,6 +1,7 @@
 # image_processing.py
 
 import argparse
+import enum
 import pathlib
 import time
 import tkinter as tk
@@ -11,6 +12,12 @@ import PIL.Image
 import PIL.ImageTk
 
 import parallel
+
+
+class ProcessingMode(enum.StrEnum):
+    PYTHON = "Python"
+    NUMPY = "NumPy"
+    PARALLEL = "Parallel (GIL-Free)"
 
 
 class AppWindow(tk.Tk):
@@ -27,10 +34,24 @@ class AppWindow(tk.Tk):
         self.frame.columnconfigure(0, weight=0)
         self.frame.columnconfigure(1, weight=1)
 
+        # Dropdown
+        self.var_mode = tk.StringVar(value=ProcessingMode.PYTHON)
+        mode_label = ttk.Label(self.frame, text="Mode:")
+        mode_label.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+        mode_dropdown = ttk.Combobox(
+            self.frame,
+            textvariable=self.var_mode,
+            values=list(ProcessingMode),
+            state="readonly",
+        )
+        mode_dropdown.grid(
+            row=0, column=1, sticky=tk.W + tk.E, padx=10, pady=10
+        )
+
         # EV slider
         self.var_ev = tk.DoubleVar(value=0)
         ev_label = ttk.Label(self.frame, text="Exposure:")
-        ev_label.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+        ev_label.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
         ev_slider = ttk.Scale(
             self.frame,
             from_=-1,
@@ -39,12 +60,12 @@ class AppWindow(tk.Tk):
             variable=self.var_ev,
         )
         ev_slider.bind("<B1-Motion>", self.on_slide)
-        ev_slider.grid(row=0, column=1, sticky=tk.W + tk.E, padx=10, pady=10)
+        ev_slider.grid(row=1, column=1, sticky=tk.W + tk.E, padx=10, pady=10)
 
         # Gamma slider
         self.var_gamma = tk.DoubleVar(value=1)
         gamma_label = ttk.Label(self.frame, text="Gamma:")
-        gamma_label.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
+        gamma_label.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
         gamma_slider = ttk.Scale(
             self.frame,
             from_=0.1,
@@ -54,7 +75,7 @@ class AppWindow(tk.Tk):
         )
         gamma_slider.bind("<B1-Motion>", self.on_slide)
         gamma_slider.grid(
-            row=1, column=1, sticky=tk.W + tk.E, padx=10, pady=10
+            row=2, column=1, sticky=tk.W + tk.E, padx=10, pady=10
         )
 
         # Image preview
@@ -64,7 +85,10 @@ class AppWindow(tk.Tk):
         # Status bar
         self.var_status = tk.StringVar()
         status_bar = ttk.Label(
-            self, anchor=tk.W, relief=tk.SUNKEN, textvariable=self.var_status
+            self,
+            anchor=tk.W,
+            relief=tk.SUNKEN,
+            textvariable=self.var_status,
         )
         status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
@@ -82,8 +106,7 @@ class AppWindow(tk.Tk):
 
         # Process pixels
         t1 = time.perf_counter()
-        pixels = self.pixels.copy()
-        parallel.process(pixels, ev, gamma)
+        pixels = self.process(ev, gamma)
         t2 = time.perf_counter()
 
         # Render preview
@@ -110,6 +133,18 @@ class AppWindow(tk.Tk):
         self.preview.configure(image=image_tk)
         self.preview.image = image_tk
 
+    def process(self, ev: float, gamma: float) -> np.ndarray:
+        match mode := self.var_mode.get():
+            case ProcessingMode.PYTHON:
+                return process_python(self.pixels, ev, gamma)
+            case ProcessingMode.NUMPY:
+                return process_numpy(self.pixels, ev, gamma)
+            case ProcessingMode.PARALLEL:
+                parallel.process(pixels := self.pixels.copy(), ev, gamma)
+                return pixels
+            case _:
+                raise ValueError(f"Invalid mode: {mode}")
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -120,6 +155,24 @@ def parse_args() -> argparse.Namespace:
 def main(args: argparse.Namespace) -> None:
     with PIL.Image.open(args.image_path) as image:
         AppWindow(image)
+
+
+def process_python(pixels: np.ndarray, ev: float, gamma: float) -> np.ndarray:
+    lookup_table = [
+        int(min(max(0, (((i / 255.0) * ev) ** gamma) * 255), 255))
+        for i in range(256)
+    ]
+    values = [lookup_table[x] for x in pixels.flat]
+    return np.array(values).astype(np.uint8).reshape(pixels.shape)
+
+
+def process_numpy(pixels: np.ndarray, ev: float, gamma: float) -> np.ndarray:
+    lookup_table = (
+        ((((np.arange(256) / 255.0) * ev) ** gamma) * 255)
+        .clip(0, 255)
+        .astype(np.uint8)
+    )
+    return lookup_table[pixels]
 
 
 if __name__ == "__main__":
