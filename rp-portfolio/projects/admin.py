71a28cffd7bfa7a49415eea60e881741309effac
Philipp Acsany <68116180+acsany@users.noreply.github.com>
2023-08-29 22:36:56 +0200

    Add updated files for the rp-portfolio update (#428)
    * Remove old materials
    
    * Add updated materials
    
    * Add Readme
    
    * Update link to folder in Readme file
    
    ---------
    
    Co-authored-by: Geir Arne Hjelle <geirarne@gmail.com>

--- rp-portfolio/projects/admin.py
+++ rp-portfolio/projects/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from projects.models import Project
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project, ProjectAdmin)
