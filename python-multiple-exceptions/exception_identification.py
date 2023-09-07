5c0311cd01db9a05d019954a51fc8649adfcf29f
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-09-07 20:00:56 +0100

    Update exception_identification.py
    Error replaced err

--- python-multiple-exceptions/exception_identification.py
+++ python-multiple-exceptions/exception_identification.py
@@ -4,5 +4,5 @@ try:
     first = float(input("What is your first number? "))
     second = float(input("What is your second number? "))
     print(f"{first} divided by {second} is {first / second}")
-except (ValueError, ZeroDivisionError) as err:
-    print(f"A {type(err).__name__} has occurred.")
+except (ValueError, ZeroDivisionError) as error:
+    print(f"A {type(error).__name__} has occurred.")
