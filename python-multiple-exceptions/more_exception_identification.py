3184c96a46deeb9bc68752ca5ecba41e0ae0ff58
eyrei123 <88923476+eyrei123@users.noreply.github.com>
2023-09-07 20:02:25 +0100

    Update more_exception_identification.py
    Error replaced err

--- python-multiple-exceptions/more_exception_identification.py
+++ python-multiple-exceptions/more_exception_identification.py
@@ -17,14 +17,14 @@ try:
         answer = calculate(truediv, first, second)
     else:
         raise RuntimeError(f"'{operation}' is an unsupported operation")
-except (RuntimeError, ValueError, ZeroDivisionError) as err:
-    print(f"A {type(err).__name__} has occurred")
-    match err:
+except (RuntimeError, ValueError, ZeroDivisionError) as error:
+    print(f"A {type(error).__name__} has occurred")
+    match error:
         case RuntimeError():
-            print(f"You have entered an invalid symbol: {err}")
+            print(f"You have entered an invalid symbol: {error}")
         case ValueError():
-            print(f"You have not entered a number: {err}")
+            print(f"You have not entered a number: {error}")
         case ZeroDivisionError():
-            print(f"You can't divide by zero: {err}")
+            print(f"You can't divide by zero: {error}")
 else:
     print(f"{first} {operation} {second} = {answer}")
