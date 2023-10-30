852d2b6ee701c797373111b0afbc75e7f246bd9c
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-10-30 16:01:35 +0100

    Fix linter issues

--- python-double-underscore/article_v1.py
+++ python-double-underscore/article_v1.py
@@ -17,23 +17,3 @@ class Article:
     def compute_age(self):
         now = datetime.datetime.now()
         return now - self.pub_date
-
-
-class Article:
-    def __init__(self, title, author, pub_date=None):
-        self.title = title
-        self.author = author
-        self.pub_date = self.__update_pub_date(pub_date)
-
-    def update_pub_date(self, pub_date=None):
-        if pub_date is None:
-            date = datetime.datetime.now()
-        else:
-            date = datetime.datetime.fromisoformat(pub_date)
-        return date
-
-    __update_pub_date = update_pub_date
-
-    def compute_age(self):
-        now = datetime.datetime.now()
-        return now - self.pub_date
