d9a13230799e3a54d57b7402b034ea56fcc3acf5
Geir Arne Hjelle <geirarne@gmail.com>
2023-11-07 16:45:30 +0100

    Final QA (#457)

--- embeddings-and-vector-databases-with-chromadb/cosine_similarity.py
+++ embeddings-and-vector-databases-with-chromadb/cosine_similarity.py
@@ -4,4 +4,4 @@ import numpy as np
 def compute_cosine_similarity(u: np.ndarray, v: np.ndarray) -> float:
     """Compute the cosine similarity between two vectors"""
 
-    return u.dot(v) / (np.linalg.norm(u) * np.linalg.norm(v))
+    return (u @ v) / (np.linalg.norm(u) * np.linalg.norm(v))
