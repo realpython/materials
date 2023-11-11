e658370aa179bbcdc29e06c4cb40e9c7c94e32c9
Rob Alford <robalford@users.noreply.github.com>
2023-11-11 01:10:01 -0800

    Project code for django markdown tutorial (#459)
    * project code for django markdown tutorial
    
    * sort import statements with isort
    
    * move readme file

---
+++ django-markdown/django_markdown/django_markdown_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import MarkdownContent
+
+
+def markdown_content_view(request, slug):
+    markdown_content = get_object_or_404(MarkdownContent, slug=slug)
+    context = {"markdown_content": markdown_content}
+    return render(
+        request, "django_markdown_app/markdown_content.html", context=context
+    )
