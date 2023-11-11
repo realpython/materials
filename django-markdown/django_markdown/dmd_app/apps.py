e2d68eeb0b7b25ef4fc003975fd30e9ab56253cc
gahjelle <geirarne@gmail.com>
2023-11-11 10:35:45 +0100

    Final QA

--- django-markdown/django_markdown/dmd_app/apps.py
+++ django-markdown/django_markdown/dmd_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoMarkdownAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_markdown_app"
+    name = "dmd_app"
--- django-markdown/django_markdown/dmd_project/wsgi.py
+++ django-markdown/django_markdown/dmd_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_markdown project.
+WSGI config for dmd_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_markdown.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmd_project.settings")
 
 application = get_wsgi_application()
