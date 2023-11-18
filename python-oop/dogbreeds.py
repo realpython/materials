b99bf2c0ee15c0347e5f7538c5a502fe5de8cbf1
Jatin Nama <118289270+jatinnama7@users.noreply.github.com>
2023-11-18 07:43:38 +0530

    Update dogbreeds.py

--- python-oop/dogbreeds.py
+++ python-oop/dogbreeds.py
@@ -1,3 +1,4 @@
+# Heirarchical Inheritance in Python -->
 class Dog:
     species = "Canis familiaris"
 
@@ -23,3 +24,29 @@ class Dachshund(Dog):
 
 class Bulldog(Dog):
     pass
+
+# Try it yourself first ;)
+
+# Creating instances of the classes
+dog = Dog("Buddy", 5)
+jack = JackRussellTerrier("Jack", 3)
+dachshund = Dachshund("Daisy", 4)
+bulldog = Bulldog("Rocky", 6)
+
+# Printing information about the instances
+print(dog.species)              # Output: Canis familiaris
+print(dog)                      # Output: Buddy is 5 years old
+print(dog.speak("Woof"))        # Output: Buddy barks: Woof
+
+
+print(jack.species)             # Output: Canis familiaris
+print(jack)                     # Output: Jack is 3 years old
+print(jack.speak())             # Output: Jack barks: Arf
+
+print(dachshund.species)        # Output: Canis familiaris
+print(dachshund)                # Output: Daisy is 4 years old
+print(dachshund.speak("Ruff"))  # Output: Daisy barks: Ruf
+
+print(bulldog.species)          # Output: Canis familiaris
+print(bulldog)                  # Output: Rocky is 6 years old
+print(bulldog.speak("Grr"))     # Output: Rocky barks: Grr
