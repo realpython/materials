b446eee27d2d588309286c17333a9cfc47072f1b
PRO X <bredalisgautreaux@gmail.com>
2023-11-20 09:05:19 -0400

    guess_final.py
    Add materials for ChatGPT Mentor tutorial (#388)

--- chatgpt-mentor/guess_final.py
+++ chatgpt-mentor/guess_final.py
@@ -16,14 +16,21 @@ def guess_the_number(low: int = 1, high: int = 100) -> int:
     """
     random_number = random.randint(low, high)
     user_guess = None
+    attempts = 0
 
     while user_guess != random_number:
-        user_guess = int(input(f"Guess a number between {low} and {high}: "))
+        try:
+            user_guess = int(input(f"Guess a number between {low} and {high}: "))
+        except ValueError:
+            print("Just integers\n")
+            continue
+
+        attempts += 1
         if user_guess < random_number:
-            print("Your guess is too low. Try again!")
+            print("Your guess is too low. Try again!\n")
         elif user_guess > random_number:
-            print("Your guess is too high. Try again!")
+            print("Your guess is too high. Try again!\n")
         else:
-            print("Congratulations, you guessed the number correctly!")
+            print(f"Congratulations, you guessed the number correctly in {attempts} attempts\n!")
 
     return random_number
