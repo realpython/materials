551cce9c65c2096c5cef1d929fa2d76c5f210c99
KateFinegan <95366190+KateFinegan@users.noreply.github.com>
2023-12-19 15:38:46 -0700

    Language edit

--- python-magic-methods/distance.py
+++ python-magic-methods/distance.py
@@ -10,7 +10,7 @@ class Distance:
         self.value = value
         self.unit = unit.lower()
 
-    def to_metre(self):
+    def to_meter(self):
         return self.value * type(self)._multiples[self.unit]
 
     def __add__(self, other):
@@ -20,7 +20,7 @@ class Distance:
         return self._compute(other, "-")
 
     def _compute(self, other, operator):
-        operation = eval(f"{self.to_metre()} {operator} {other.to_metre()}")
+        operation = eval(f"{self.to_meter()} {operator} {other.to_meter()}")
         cls = type(self)
         return cls(operation / cls._multiples[self.unit], self.unit)
 
