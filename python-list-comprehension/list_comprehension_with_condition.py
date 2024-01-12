1749fbf9403b778aa7d8c4dc959f7920e453997e
gahjelle <geirarne@gmail.com>
2024-01-12 13:34:59 +0100

    Add materials for the list comprehension update

---
+++ python-list-comprehension/list_comprehension_with_condition.py
@@ -0,0 +1,3 @@
+sentence = "the rocket came back from mars"
+vowels = [char for char in sentence if char in "aeiou"]
+print(vowels)
---
+++ python-list-comprehension/set_comprehension.py
@@ -0,0 +1,2 @@
+squares = {i: i * i for i in range(10)}
+print(squares)
