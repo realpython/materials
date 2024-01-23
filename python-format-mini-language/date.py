5f43b12cb540dc8b905995a687cbb8726f1faee8
martin-martin <breuss.martin@gmail.com>
2024-01-23 11:24:50 -0400

    Add month, rename file

--- python-format-mini-language/date.py
+++ python-format-mini-language/date.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 now = datetime.now()
 
-print(f"Today is {now:%a %d, %Y} and it's {now:%H:%M} hours")
+print(f"Today is {now:%a %b %d, %Y} and it's {now:%H:%M} hours")
