32cc5faccc39b91cc1cb413ac010bd69c4073969
hfhoffman1144 <hfh160130@utdallas.edu>
2024-02-10 16:25:16 -0600

    flake8 update

--- langchain-rag-app/chatbot_frontend/src/main.py
+++ langchain-rag-app/chatbot_frontend/src/main.py
@@ -10,10 +10,12 @@ with st.sidebar:
     st.header("About")
     st.markdown(
         """
-        This chatbot interfaces with a [LangChain](https://python.langchain.com/docs/get_started/introduction) agent
-        designed to answer questions about the hospitals, patients, visits, physicians, and insurance payers in
-        a fake hospital system. The agent uses retrieval-augment generation (RAG)
-        over both structured and unstructured data that has been synthetically generated.
+        This chatbot interfaces with a
+        [LangChain](https://python.langchain.com/docs/get_started/introduction)
+        agent designed to answer questions about the hospitals, patients,
+        visits, physicians, and insurance payers in  a fake hospital system.
+        The agent uses  retrieval-augment generation (RAG) over both
+        structured and unstructured data that has been synthetically generated.
         """
     )
 
