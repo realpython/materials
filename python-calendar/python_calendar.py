8323e0f9710e4220ca8ba92c09a9e1914b993519
robalford <robertsalford@gmail.com>
2024-04-03 12:47:28 -0700

    address flake8 issues

--- python-calendar/python_calendar.py
+++ python-calendar/python_calendar.py
@@ -25,30 +25,35 @@ text_calendar.prmonth(2024, 1)
 locale_calendar = calendar.LocaleTextCalendar(locale="de_DE.UTF-8")
 locale_calendar.prmonth(2024, 1)
 
-"""Print weekday number values in order with Sunday (6) as the first day of the week"""
+"""Print weekday number values in order with Sunday (6) as the first
+day of the week"""
 
 for day_number in text_calendar.iterweekdays():
     print(day_number)
 
-"""Print all dates of January 2024 calendar as strings including dates from previous and following months
-needed to complete the first and last weeks"""
+"""Print all dates of January 2024 calendar as strings including dates from
+previous and following months needed to complete the first and last weeks"""
 
 for day in text_calendar.itermonthdates(2024, 1):
     day.strftime("%m/%d/%Y")
 
-"""Print all dates of January 2024 calendar as numerical days of the month including dates from previous 
-and following months needed to complete the first and last weeks represented as zeros"""
+"""Print all dates of January 2024 calendar as numerical days of the month
+including dates from previous and following months needed to complete the
+first and last weeks represented as zeros"""
 
 for day in text_calendar.itermonthdays(2024, 1):
     print(day)
 
-"""Print all dates of January 2024 calendar as tuples containing the day of the month and day of the week 
-as numbers"""
+"""Print all dates of January 2024 calendar as tuples containing the day of
+the month and day of the week as numbers"""
+
 for day in text_calendar.itermonthdays2(2024, 1):
     print(day)
 
 """Check if a given year is a leap year"""
+
 calendar.isleap(2024)
 
 """Return the number of leap years between two given years"""
+
 calendar.leapdays(1980, 2024)
