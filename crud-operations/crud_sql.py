f930e37762ae3ffd17b8649da854f9b1585c59b5
Philipp <filip.axani+github@gmail.com>
2024-04-27 13:27:38 +0200

    Update materials after TR

--- crud-operations/crud_sql.py
+++ crud-operations/crud_sql.py
@@ -6,12 +6,10 @@ def connect_to_db(db_path):
 
 
 if __name__ == "__main__":
-    connection = connect_to_db("birds.db")
-    create_bird_table = """
-    CREATE TABLE IF NOT EXISTS bird (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL
-    );
-    """
-    connection.execute(create_bird_table)
-    connection.close()
+    with connect_to_db("birds.db") as connection:
+        connection.execute("""
+          CREATE TABLE IF NOT EXISTS bird (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+          );
+        """)
