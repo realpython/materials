cee21e7ba59612c1631d06c5de7837333198e6c1
Leodanis Pozo Ramos <lpozor78@gmail.com>
2024-08-23 16:45:18 +0200

    Sample code for the article on string formatting tools

---
+++ python-formatted-output/README.md
@@ -0,0 +1,3 @@
+# A Guide to the Newer Python String Format Techniques
+
+This folder provides the code examples for the Real Python tutorial [A Guide to the Newer Python String Format Techniques](https://realpython.com/python-formatted-output/).
---
+++ python-formatted-output/person.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"I'm {self.name}, and I'm {self.age} years old."
+
+    def __repr__(self):
+        return f"{type(self).__name__}(name='{self.name}', age={self.age})"
+
+
+jane = Person("Jane", 25)
+f"{jane!s}"
+f"{jane!r}"
+"{jane!s}".format(jane=jane)
+"{jane!r}".format(jane=jane)
