67902f179dbb4a6b15c94db62dd98c79698a0556
Charles de Villiers <chasdev@gmail.com>
2024-11-27 13:57:22 +0200

    Sample code for the Textual showcase article. Updated and submitted for TR with the text.

--- textual-showcase/textual-showcase/simple_layout_with_tcss.py
+++ textual-showcase/textual-showcase/simple_layout_with_tcss.py
@@ -3,20 +3,17 @@ from textual.containers import Horizontal, Vertical
 from textual.widgets import Static
 
 
-class SimpleLayoutApp(App):
+class SimpleLayoutWithTCSSApp(App):
     CSS_PATH = "simple_layout.tcss"
+
     def compose(self) -> ComposeResult:
         with Vertical():
-            self.widget1 = Static("   :point_up:\nCentered", id="widget1")
-            yield self.widget1
+            yield Static("   :point_up:\nCentered", id="widget1")
             with Horizontal():
-                self.widget2 = Static(":point_left: Left Aligned", id="widget2")
-                yield self.widget2
-                self.widget3 = Static("Right Aligned :point_right:", id="widget3")
-                yield self.widget3
+                yield Static(":point_left: Left Aligned", id="widget2")
+                yield Static("Right Aligned :point_right:", id="widget3")
 
-    
 
 if __name__ == "__main__":
-    app = SimpleLayoutApp()
+    app = SimpleLayoutWithTCSSApp()
     app.run()
--- textual-showcase/textual-showcase/tabbed_layout.py
+++ textual-showcase/textual-showcase/tabbed_layout.py
@@ -1,67 +1,92 @@
-# from rich.text import Text
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, Vertical
-from textual.widgets import Header, Label, SelectionList, Sparkline, TabbedContent, Tree
+from textual.containers import Horizontal, Vertical, VerticalScroll
+from textual.widgets import (
+    Header,
+    Label,
+    SelectionList,
+    Sparkline,
+    TabbedContent,
+    Tree,
+)
 
 
 class TabbedLayoutApp(App):
-    CSS = """
+    DEFAULT_CSS = """
+    Sparkline {
+    width: 80%;
+    margin:1 2;
+    }
 
- Sparkline {
- width: 100%;
- margin:1;
- }
+    #pi > .sparkline--max-color {
+        color: $success;
+    }
+    #e > .sparkline--max-color {
+        color: $warning;
+    } 
+    #ln2 > .sparkline--max-color {
+        color: $error;
+    } 
 
- #pi > .sparkline--max-color {
-     color: $success;
- }
- #e > .Sparkline--max_color {
-     color: $warning;
- } 
- #ln2 > .Sparkline--max_color {
-     color: red;
- } 
-
- Vertical > Label {
-    color: yellow;
-    align: center middle;
- }
- 
- Grid {
- grid-size: 3 2
- }
+    Horizontal > Label {
+        color: black;
+        background: orange;
+        align: center middle;
+        margin: 0 0 2 1;
+    }
 """
 
     def compose(self) -> ComposeResult:
         yield Header("Tabbed Layout")
-        with TabbedContent("Options", "Tree", "Sparklines"):
-            yield SelectionList(
-                (":beer: Beer", 1, True),
-                (":wine_glass: Wine", 2),
-                (":cocktail: Cocktails", 3),
-                (":pizza: Pizza", 4, True),
-                (":hamburger: Burgers", 5),
-                (":fries: Fries", 6),
-                (":hot_dog: Hot Dogs", 7),
-                (":taco: Tacos", 8),
-                (":burrito: Burritos", 9),
-                (":popcorn: Popcorn", 10),
-            )
-            yield self.build_tree()
+        with TabbedContent("Transport", "Life", "Transcendentals"):
+            with VerticalScroll():
+                yield self.build_transport_selector()
+            yield self.build_tree_of_life()
             with Vertical():
-                with Horizontal():
-                    yield Label("Pi")
-                    yield Sparkline([3, 1, 4, 1, 5, 9, 2, 6, 5, 3], id="pi")
-                with Horizontal():
-                    yield Label("e")
-                    yield Sparkline([6, 2, 1, 8, 2, 8, 1, 8, 2, 8], id="e")
-                with Horizontal():
-                    yield Label("ln(2)")
-                    yield Sparkline([0, 6, 9, 3, 1, 4, 7, 1, 8, 0, 6], id="ln2")
+                yield Label("Pi = 3.1415926535...")
+                yield Sparkline([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5], id="pi")
+                yield Label("e = 2.718281828...")
+                yield Sparkline([2, 7, 1, 8, 2, 8, 1, 8, 2, 8], id="e")
+                yield Label("ln(2) = 0.6931471806...")
+                yield Sparkline([0, 6, 9, 3, 1, 4, 7, 1, 8, 0, 6], id="ln2")
+                yield Label("Omega = 0.5671432904...")
+                yield Sparkline([0, 5, 6, 7, 1, 4, 3, 2, 9, 0, 4], id="omega")
 
-    def build_tree(self):
+    def build_transport_selector(self) -> SelectionList[int]:
+        return SelectionList[int](
+            (":airplane: Airplane", 1,),
+            (":ambulance: Ambulance", 2,),
+            (":articulated_lorry: Lorry", 3,),
+            (":auto_rickshaw: Rickshaw", 4,),
+            (":automobile: Automobile", 5,),
+            (":bicycle: Bicycle", 6,),
+            (":blue_car: Blue Car", 7),
+            (":broom: Broomstick", 8),
+            (":car: Red Car", 9),
+            (":delivery_truck: Delivery Truck", 10),
+            (":dromedary_camel: Camel", 11),
+            (":elephant: Elephant", 12),
+            (":fire_engine: Fire Engine", 13),
+            (":flying_saucer: UFO", 14),
+            (":helicopter: Helicopter", 15),
+            (":minibus: Minibus", 16),
+            (":motorcycle: Motorcycle", 17),
+            (":motor_scooter: Scooter", 18),
+            (":police_car: Police Car", 19),
+            (":rocket: Rocket", 20),
+            (":sailboat: Sailboat", 21),
+            (":ship: Ship", 22),
+            (":speedboat: Speedboat", 23),
+            (":steam_locomotive: Locomotive", 24),
+            (":taxi: Taxi", 25),
+            (":tractor: Tractor", 26),
+            (":train: Train", 27),
+            (":trolleybus: Trolleybus", 28),
+            (":truck: Truck", 29),
+        )
+    def build_tree_of_life(self) -> Tree[dict]:
         tree: Tree[dict] = Tree("Life")
-        domains = tree.root.add("Domains", expand=True)
+        domains = tree.root.add_leaf("Domains",)
+        domains.allow_expand = True
         domains.add_leaf("Bacteria")
 
         archaea = domains.add_leaf("Archaea")
@@ -69,16 +94,26 @@ class TabbedLayoutApp(App):
         archaea.add_leaf("Euryarcheota")
         archaea.add_leaf("DPANN")
         archaea.allow_expand = True
-
+    
         eukarya = domains.add_leaf("Eukarya")
         eukarya.add_leaf("Protista")
         eukarya.add_leaf("Plantae")
         eukarya.add_leaf("Fungi")
-        eukarya.add_leaf("Animalia")
+        animalia = eukarya.add_leaf("Animalia")
+        animalia.add_leaf("Chordata")
+        animalia.add_leaf("Arthropoda")
+        animalia.add_leaf("Mollusca")
+        animalia.add_leaf("Cnidaria")
+        animalia.add_leaf("Platyhelminthes")
+        animalia.add_leaf("Nematoda")
+        animalia.add_leaf("Annelida")
+        animalia.add_leaf("Echinodermata")
+        animalia.add_leaf("Porifera")
+        animalia.add_leaf("Ctenophora") 
+        animalia.allow_expand = True
         eukarya.allow_expand = True
-        tree.root.expand_all()
         return tree
-
+    
 
 if __name__ == "__main__":
     TabbedLayoutApp().run()
