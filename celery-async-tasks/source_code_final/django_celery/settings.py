af9990d0a5909842eab5daea2be952c0503c5971
martin-martin <breuss.martin@gmail.com>
2024-11-28 20:21:21 +0100

    Remove default setting

--- celery-async-tasks/source_code_final/django_celery/settings.py
+++ celery-async-tasks/source_code_final/django_celery/settings.py
@@ -131,4 +131,3 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # Celery settings
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
-CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
