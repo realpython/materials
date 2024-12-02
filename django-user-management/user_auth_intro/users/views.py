fedf4314537f61f438759b8d262d256b681b1a62
Philipp <filip.axani+github@gmail.com>
2024-12-02 14:55:24 +0100

    Remove namespaced URL relicts and delete unused file

--- django-user-management/user_auth_intro/users/views.py
+++ django-user-management/user_auth_intro/users/views.py
@@ -17,7 +17,7 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(reverse("users:dashboard"))
+            return redirect(reverse("dashboard"))
     else:
         form = CustomUserCreationForm()
     return render(request, "registration/sign_up.html", {"form": form})
