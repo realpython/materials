19b1ee1a3192013f3ae8ce2ac2743dacbbadc17a
martin-martin <breuss.martin@gmail.com>
2025-01-24 12:51:58 +0100

    Update code examples after TR

--- split-strings/split_logs_regex.py
+++
@@ -1,7 +0,0 @@
-import re
-
-log_line = "2025-01-15 08:45:23 INFO User logged in from IP 10.0.1.1"
-pattern = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\s(\w+)\s"
-components = re.split(pattern, log_line)
-
-print(components)
