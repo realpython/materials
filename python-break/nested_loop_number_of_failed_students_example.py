f4d14af6cda6b19fd8ad60df9e4ffc38ea63c4ab
dphoenix <dphoenix@users.noreply.github.com>
2025-02-04 12:12:28 -0500

    Add files via upload
    Nested loop example, which prints out the number of students who got at least one failed score

---
+++ python-break/nested_loop_number_of_failed_students_example.py
@@ -0,0 +1,15 @@
+# Use case 4: Check to see how many students have failed a test (nested loop)
+scores = [
+    [90, 30, 80, 100],
+    [100, 80, 95, 87],
+    [75, 84, 77, 90]
+]
+
+failed_score = 60
+num_failed_students = 0
+for score_set in scores:
+    for score in score_set:
+        if score < failed_score:
+            num_failed_students += 1
+            break
+print("Number of students who failed a test: " + str(num_failed_students))
