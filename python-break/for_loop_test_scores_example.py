30b6d8cea59352ae1ec3cb4cf7cdc47aabec6069
dphoenix <dphoenix@users.noreply.github.com>
2025-02-04 11:16:49 -0500

    Updated for-loop code example
    Changed variable names to use underscores, rather than camel-case

--- python-break/for_loop_test_scores_example.py
+++ python-break/for_loop_test_scores_example.py
@@ -1,14 +1,14 @@
-# Use case 1: If the student fails 2 or more tests, the student must go to tutoring (for loop)
+# Use case 1: If the student fails 2 or more tests, the student must go to tutoring (for-loop)
 scores = [90, 30, 50, 70, 85, 35]
 
-numFailedScores = 0
-failedScore = 60
-needsTutoring = False
+num_failed_scores = 0
+failed_score = 60
+needs_utoring = False
 for score in scores:
-    if score < failedScore:
-        numFailedScores += 1
-    if numFailedScores >= 2:
-        needsTutoring = True
+    if score < failed_score:
+        num_failed_scores += 1
+    if num_failed_scores >= 2:
+        needs_tutoring = True
         break
 
-print("Does the student need tutoring? " + str(needsTutoring))
+print("Does the student need tutoring? " + str(needs_tutoring))
