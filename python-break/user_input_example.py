4fa269a509a4281328117bcc2e415f760d4445c6
Philipp <filip.axani+github@gmail.com>
2025-02-05 21:27:26 +0100

    Make linter happy

--- python-break/user_input_example.py
+++ python-break/user_input_example.py
@@ -8,13 +8,16 @@ random_number = random.randint(1, 10)
 
 while True:
     if guesses_left <= 0:
-        print("You ran out of guesses! The correct number was " + str(random_number))
+        print(
+            "You ran out of guesses! The correct number was "
+            + str(random_number)
+        )
         break
     guess = input("Guess a number between 1 and 10, or enter q to quit:")
     if guess == "q":
         print("Successfully exited game.")
         break
-    elif(not(guess.isnumeric())):
+    elif not (guess.isnumeric()):
         guess = input("Please enter a valid value: ")
     else:
         if int(guess) == random_number:
