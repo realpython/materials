b393891c240ad92f093723f128df4916cac9d97f
dphoenix <dphoenix@users.noreply.github.com>
2025-02-06 11:27:07 -0500

    Update for_loop_test_scores_example.py
    Fixed a variable typo

--- python-break/for_loop_test_scores_example.py
+++ python-break/for_loop_test_scores_example.py
@@ -4,7 +4,7 @@ scores = [90, 30, 50, 70, 85, 35]
 
 num_failed_scores = 0
 failed_score = 60
-needs_utoring = False
+needs_tutoring = False
 for score in scores:
     if score < failed_score:
         num_failed_scores += 1
