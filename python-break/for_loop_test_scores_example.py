1c08fe0aa2faceb50d47db7a060d78c5dd1a3def
dphoenix <dphoenix@users.noreply.github.com>
2025-02-06 11:48:03 -0500

    Update for_loop_test_scores_example.py
    - Changed True/False values to "Yes"/"No", to make output more reader-friendly
    - Used f-string for string interpolation

--- python-break/for_loop_test_scores_example.py
+++ python-break/for_loop_test_scores_example.py
@@ -4,12 +4,12 @@ scores = [90, 30, 50, 70, 85, 35]
 
 num_failed_scores = 0
 failed_score = 60
-needs_tutoring = False
+needs_tutoring = "No"
 for score in scores:
     if score < failed_score:
         num_failed_scores += 1
     if num_failed_scores >= 2:
-        needs_tutoring = True
+        needs_tutoring = "Yes"
         break
 
-print("Does the student need tutoring? " + str(needs_tutoring))
+print(f"Does the student need tutoring? {needs_tutoring}")
