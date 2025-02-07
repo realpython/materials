d5ad22932e2574bb41fe58404b8cb8a892a4bbc2
dphoenix <dphoenix@users.noreply.github.com>
2025-02-07 14:02:27 -0500

    Update user_input_example.py
    Removed unnecessary nested loop

--- python-break/user_input_example.py
+++ python-break/user_input_example.py
@@ -8,21 +8,18 @@ random_number = random.randint(1, 10)
 
 while True:
     if guesses_left <= 0:
-        print(
-            f"You ran out of guesses! The correct number was {random_number}"
-        )
+        print(f"You ran out of guesses! The correct number was {random_number}")
         break
     guess = input("Guess a number between 1 and 10, or enter q to quit:")
     if guess == "q":
         print("Successfully exited game.")
         break
-    elif not (guess.isnumeric()):
-        guess = input("Please enter a valid value: ")
+    elif(not(guess.isnumeric())):
+        print("Please enter a valid value.")
+    elif int(guess) == random_number:
+        print("Congratulations, you picked the correct number!")
+        break
     else:
-        if int(guess) == random_number:
-            print("Congratulations, you picked the correct number!")
-            break
-        else:
-            print("Sorry, your guess was incorrect.")
-            guesses_left -= 1
-            print(f"You have {guesses_left} guesses left.")
+        print("Sorry, your guess was incorrect.")
+        guesses_left -= 1
+        print(f"You have {guesses_left} guesses left.")
