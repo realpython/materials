f62583d5d2e2f3e06dcf4fb60b70fbb6a27920fc
dphoenix <dphoenix@users.noreply.github.com>
2025-02-08 15:54:12 -0500

    Update nested_loop_number_of_failed_students_example.py
    Created a more meaningful variable name

--- python-break/nested_loop_number_of_failed_students_example.py
+++ python-break/nested_loop_number_of_failed_students_example.py
@@ -3,8 +3,8 @@ scores = [[90, 30, 80, 100], [100, 80, 95, 87], [75, 84, 77, 90]]
 
 failed_score = 60
 num_failed_students = 0
-for score_set in scores:
-    for score in score_set:
+for student_score_list in scores:
+    for score in student_score_list:
         if score < failed_score:
             num_failed_students += 1
             break
