63c4945f7fbe9731876ac0e5c3debfb8478d9989
Philipp <filip.axani+github@gmail.com>
2025-02-13 16:38:15 +0100

    Use black formatting

--- python-break/user_input_example.py
+++ python-break/user_input_example.py
@@ -8,13 +8,15 @@ random_number = random.randint(1, 10)
 
 while True:
     if guesses_left <= 0:
-        print(f"You ran out of guesses! The correct number was {random_number}")
+        print(
+            f"You ran out of guesses! The correct number was {random_number}"
+        )
         break
     guess = input("Guess a number between 1 and 10, or enter q to quit:")
     if guess == "q":
         print("Successfully exited game.")
         break
-    elif(not(guess.isnumeric())):
+    elif not (guess.isnumeric()):
         print("Please enter a valid value.")
     elif int(guess) == random_number:
         print("Congratulations, you picked the correct number!")
