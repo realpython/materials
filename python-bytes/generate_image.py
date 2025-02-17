36eeff1aa892c8fdb1103be7476ee39834adb31b
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-02-17 13:59:09 +0100

    Apply TR feedback

--- python-bytes/generate_image.py
+++ python-bytes/generate_image.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
 
-pixels = np.random.rand(1080, 1920, 3) * 255
+pixels = np.random.default_rng().random((1080, 1920, 3)) * 255
 image = Image.fromarray(pixels.astype("uint8"))
 image.save("image.png")
