21b38b3d640bccc75dc29dc00ef4981dcc984fb8
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-03-13 17:37:00 +0100

    Sample code for the article on `.__dict__`

---
+++ python-dict-attribute/person_v2.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return "{first_name} {last_name} is {age} years old".format(
+            **self.__dict__
+        )
+
+    def __repr__(self):
+        return "{cls}('{first_name}', '{last_name}', {age})".format(
+            cls=type(self).__name__,
+            **self.__dict__,
+        )
+
+    def as_dict(self):
+        return self.__dict__
+
+    def as_tuple(self):
+        return tuple(self.__dict__.values())
+
+    def from_dict(self, data):
+        self.__dict__.update(data)
+        return self
