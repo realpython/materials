59a616c10794f45cfff761fcbba4a9deeb08dad9
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-03-26 12:14:20 +0100

    More linter fixes

--- python-namespace/scopes.py
+++ python-namespace/scopes.py
@@ -1,13 +1,18 @@
 global_variable = "global"
+
+
 def outer_func():
     # Nonlocal scope
     nonlocal_variable = "nonlocal"
+
     def inner_func():
         # Local scope
         local_variable = "local"
         print(f"Hi from the '{local_variable}' scope!")
         print(f"Hi from the '{nonlocal_variable}' scope!")
         print(f"Hi from the '{global_variable}' scope!")
+
     inner_func()
+
 
 outer_func()
