893f429ef27f9ec09bd8f3310f01100efb144e00
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-04-03 10:17:35 +0200

    Update nested_loop_number_of_failed_students_example.py

--- how-to-exit-loops-early-with-python-break-keyword/nested_loop_number_of_failed_students_example.py
+++ how-to-exit-loops-early-with-python-break-keyword/nested_loop_number_of_failed_students_example.py
@@ -1,4 +1,4 @@
-scores = [[90, 30, 80, 100], [100, 80, 95, 87], [75, 84, 77, 90]]
+scores = [[90, 30, 80, 100], [100, 80, 95, 87], [75, 84, 77, 50]]
 
 failed_score = 60
 num_failed_students = 0
@@ -7,4 +7,5 @@ for student_score_list in scores:
         if score < failed_score:
             num_failed_students += 1
             break
-print("Number of students who failed a test: " + str(num_failed_students))
+
+print(f"Number of students who failed a test: {num_failed_students}")
