c6f3ff719036f8e666d6767cd4c80c1d667439e8
martin-martin <breuss.martin@gmail.com>
2025-04-24 18:34:33 +0200

    Update project code
    Add pyproject.toml,
    Add named scripts,
    Rename script,
    Remove initial versions of scripts

--- skip-ahead-with-continue/example1.py
+++
@@ -1,6 +0,0 @@
-total = 0
-
-for number in range(-10, 10):
-    total += number
-
-print(total)
--- skip-ahead-with-continue/example1_continued.py
+++
@@ -1,8 +0,0 @@
-total = 0
-
-for number in range(-10, 10):
-    if number < 0:
-        continue
-    total += number
-
-print(total)
