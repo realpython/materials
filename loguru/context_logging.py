f8907121534d69599f2cadaca50fba8f7b569e99
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-04-30 13:59:13 +0200

    Post-TR updates

--- loguru/context_logging.py
+++ loguru/context_logging.py
@@ -5,7 +5,7 @@ from loguru import logger
 logger.remove()
 logger.add(sys.stderr, format="{time} | {level} | {message} | {extra}")
 
-user_logger = logger.bind(user_id="123")
+user_logger = logger.bind(user_id=123)
 user_logger.info("User logged in")
 user_logger.info("User started a session")
 
