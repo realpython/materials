a81b17ded60e0276654b1a904592990a6d649b6d
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-05-19 14:26:01 +0200

    Remove file

--- python-function/rectangle.py
+++
@@ -1,5 +0,0 @@
-def rectangle_area(
-    width: {"description": "Width of rectangle", "type": float},
-    height: {"description": "Height of rectangle", "type": float},
-) -> {"description": "Area of rectangle", "type": float}:
-    return width * height
