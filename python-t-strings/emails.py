2e1c1384fdb5b9a5fa8a6e83fd4bf4bfa6eb89a2
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-06-02 16:28:44 +0200

    Again

--- python-t-strings/emails.py
+++ python-t-strings/emails.py
@@ -1,9 +1,9 @@
 def fill_order_template(lang, name, order_id, date):
     "Uncomment in Python 3.14+"
     # templates = {
-    #     "en": t"Thank you, {name}! Your order #{order_id} will arrive on {date}.",  # noqa
-    #     "es": t"Gracias, {name} Su orden No. {order_id} arribará el próximo {date}.",  # noqa
-    #     "fr": t"Merci, {name}! Votre commande n° {order_id} arrivera le {date}.",  # noqa
+    #     "en": t"Thank you, {name}! Your order #{order_id} will arrive on {date}.",
+    #     "es": t"Gracias, {name} Su orden No. {order_id} arribará el próximo {date}.",
+    #     "fr": t"Merci, {name}! Votre commande n° {order_id} arrivera le {date}.",
     # }
     # return templates.get(lang, templates["en"])
 
