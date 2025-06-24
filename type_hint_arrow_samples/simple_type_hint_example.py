4f7e271ac1db1b325d2b176161af81084a34f0d1
dphoenix <dphoenix@users.noreply.github.com>
2025-06-24 09:34:56 -0400

    Create simple_type_hint_example.py

---
+++ type_hint_arrow_samples/simple_type_hint_example.py
@@ -0,0 +1,5 @@
+def get_number_of_titles(list) -> int:
+    return len(list)
+
+game_titles_list = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
+print(f"Number of titles: {get_number_of_titles(game_titles_list)}") 
