def2f9eff54aaa896133a4b9a5b022c4aa0b8017
dphoenix <dphoenix@users.noreply.github.com>
2025-06-24 09:35:44 -0400

    Add files via upload

---
+++ type_hint_arrow_samples/dictionary_return_type_example.py
@@ -0,0 +1,20 @@
+game_stock = {
+    "Dragon Quest": 5,
+    "Final Fantasy": 1,
+    "Age of Empires": 5
+}
+
+def get_highest_stock_game() -> dict[str, int]:
+    stock_quantities = game_stock.values()
+    high = 0
+    for quantity in stock_quantities:
+        if quantity > high:
+            high = quantity
+    game_results = {}
+    
+    for game in game_stock:
+        if game_stock[game] == high:
+            game_results[game] = high
+    return game_results
+
+print(f"Games with the highest stock: {get_highest_stock_game()}")
---
+++ type_hint_arrow_samples/str_return_type_hint_example.py
@@ -0,0 +1,5 @@
+def get_game_recommendation(list) -> str:
+    return random.choice(list)
+
+game_titles_list = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
+print(f"Random recommendation: {get_game_recommendation(game_titles_list)}")
