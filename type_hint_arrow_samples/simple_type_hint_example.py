5a6b4b7f1cc5c0bb7140a1e100f1fd9fcac43b0d
dphoenix <dphoenix@users.noreply.github.com>
2025-07-06 18:31:07 -0400

    Update simple_type_hint_example.py

--- type_hint_arrow_samples/simple_type_hint_example.py
+++ type_hint_arrow_samples/simple_type_hint_example.py
@@ -1,5 +1,5 @@
-def get_number_of_titles(list) -> int:
-    return len(list)
+def get_number_of_titles(titles: list) -> int:
+    return len(titles)
 
 
 game_titles_list = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
