02f20991a5071b25a0808840dac8b2446ba2a242
dphoenix <dphoenix@users.noreply.github.com>
2025-07-06 19:16:45 -0400

    Update mismatched_return_type_example.py

--- type_hint_arrow_samples/mismatched_return_type_example.py
+++ type_hint_arrow_samples/mismatched_return_type_example.py
@@ -4,4 +4,3 @@ def get_number_of_titles(titles: list) -> str:
 
 game_titles_list = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
 print(f"Number of titles: {get_number_of_titles(game_titles_list)}")
-
