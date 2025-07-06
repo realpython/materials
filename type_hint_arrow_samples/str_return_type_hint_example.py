c5a15adf3c82dbe27d2e224e20ccecf23d2c56cb
dphoenix <dphoenix@users.noreply.github.com>
2025-07-06 17:19:05 -0400

    Update str_return_type_hint_example.py

--- type_hint_arrow_samples/str_return_type_hint_example.py
+++ type_hint_arrow_samples/str_return_type_hint_example.py
@@ -1,1 +1,7 @@
+import random
 
+def get_game_recommendation(titles: list) -> str:
+    return random.choice(titles)
+
+game_titles_list = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
+print(f"Random recommendation: {get_game_recommendation(game_titles_list)}")
