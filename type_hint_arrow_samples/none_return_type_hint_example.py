32eb1712b3ae469a489f2a759435f658e9fb9c1a
dphoenix <dphoenix@users.noreply.github.com>
2025-07-06 17:48:23 -0400

    Update none_return_type_hint_example.py

--- type_hint_arrow_samples/none_return_type_hint_example.py
+++ type_hint_arrow_samples/none_return_type_hint_example.py
@@ -4,5 +4,6 @@ def find_keyword_in_titles(list, keyword) -> None:
         if keyword in game_title:
             print(f"{game_title}")
 
+
 game_titles_list = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
-find_keyword_in_titles(game_titles_list, "Final")  
+find_keyword_in_titles(game_titles_list, "Final")
