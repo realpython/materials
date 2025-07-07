0b936c87cb5a0ef5669a29aab0f48fddaff2859e
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-07-07 15:52:31 +0200

    Post final QA fixes

--- debug-python-errors/test_fruit.py
+++ debug-python-errors/test_fruit.py
@@ -3,35 +3,37 @@ import unittest
 from fruit import capitalize_fruit_names
 
 
-class TestAllFruits(unittest.TestCase):
+class TestFruit(unittest.TestCase):
     def test_empty_list(self):
         """with empty list"""
         self.assertEqual(capitalize_fruit_names([]), [])
 
-    def test_lowercase_list(self):
+    def test_lowercase(self):
         """with lowercase strings"""
         self.assertEqual(
             capitalize_fruit_names(["apple", "banana", "cherry"]),
             ["Apple", "Banana", "Cherry"],
         )
 
-    def test_uppercase_list(self):
+    def test_uppercase(self):
         """with uppercase strings"""
         self.assertEqual(
             capitalize_fruit_names(["APPLE", "BANANA", "CHERRY"]),
             ["Apple", "Banana", "Cherry"],
         )
 
-    def test_mixed_case_list(self):
+    def test_mixed_case(self):
         """with mixed case strings"""
         self.assertEqual(
-            capitalize_fruit_names(["mAnGo", "grApE"]), ["Mango", "Grape"]
+            capitalize_fruit_names(["mAnGo", "grApE"]),
+            ["Mango", "Grape"],
         )
 
     def test_non_string_element(self):
         """with a mix of integer and string elements"""
         self.assertEqual(
-            capitalize_fruit_names([123, "banana"]), ["", "Banana"]
+            capitalize_fruit_names([123, "banana"]),
+            ["", "Banana"],
         )
 
 
