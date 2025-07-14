0f1d69554c8aab2f1185ce809dad0f039d56d2c8
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-07-14 15:55:01 +0200

    Post final QA fixes

--- python-isinstance/players_iterable_tests.py
+++ python-isinstance/players_iterable_tests.py
@@ -10,20 +10,31 @@ from player_iterables import (
 for player in PlayersVersionOne(["Fast Ed", "Slow Jo", "Still Su"]):
     print(player)
 
-isinstance(PlayersVersionOne(["Fast Ed", "Slow Jo", "Still Su"]), Iterable)
+print(
+    f"{isinstance(PlayersVersionOne(["Fast Ed", "Slow Jo", "Still Su"]), Iterable) = }"
+)
 
+print()
 for player in PlayersVersionTwo(["Fast Ed", "Slow Jo", "Still Su"]):
     print(player)
 
-isinstance(PlayersVersionTwo(["Fast Ed", "Slow Jo", "Still Su"]), Iterable)
+print(
+    f"{isinstance(PlayersVersionTwo(["Fast Ed", "Slow Jo", "Still Su"]), Iterable) = }"
+)
 
+print()
 for player in PlayersVersionThree(["Fast Ed", "Slow Jo", "Still Su"]):
     print(player)
 
-isinstance(PlayersVersionThree(["Fast Ed", "Slow Jo", "Still Su"]), Iterable)
+print(
+    f"{isinstance(PlayersVersionThree(["Fast Ed", "Slow Jo", "Still Su"]), Iterable) = }"
+)
 
 # This will fail.
+print()
 for player in PlayersVersionFour(["Fast Ed", "Slow Jo", "Still Su"]):
     print(player)
 
-isinstance(PlayersVersionFour(["Fast Ed", "Slow Jo", "Still Su"]), Iterable)
+print(
+    f"{isinstance(PlayersVersionFour(["Fast Ed", "Slow Jo", "Still Su"]), Iterable) = }"
+)
