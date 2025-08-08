c13c9d767455b85c17d731a17187fba8978d2108
dphoenix <dphoenix@users.noreply.github.com>
2025-08-08 16:00:47 -0400

    Update none_return_type_hint_example.py
    Added type for list

--- type-hint-arrow-samples/none_return_type_hint_example.py
+++ type-hint-arrow-samples/none_return_type_hint_example.py
@@ -1,4 +1,4 @@
-def find_keyword_in_titles(titles: list, keyword: str) -> None:
+def find_keyword_in_titles(titles: list[str], keyword: str) -> None:
     print(f"Titles that contain the keyword {keyword}")
     for game_title in titles:
         if keyword in game_title:
@@ -7,3 +7,4 @@ def find_keyword_in_titles(titles: list, keyword: str) -> None:
 
 games = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
 find_keyword_in_titles(games, "Final")
+
