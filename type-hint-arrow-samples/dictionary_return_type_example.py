7a602e4d1fc6ad1d44076ea38d192f6aeb75239e
dphoenix <dphoenix@users.noreply.github.com>
2025-08-08 16:11:22 -0400

    Update dictionary_return_type_example.py

--- type-hint-arrow-samples/dictionary_return_type_example.py
+++ type-hint-arrow-samples/dictionary_return_type_example.py
@@ -1,6 +1,5 @@
 game_stock = {"Dragon Quest": 5, "Final Fantasy": 1, "Age of Empires": 5}
 
-
 def get_highest_stock_games(game_stock: dict[str, int]) -> dict[str, int]:
     max_quantity = max(game_stock.values())
     return {game: quantity for game, quantity in game_stock.items()
