9c216a960bfa00c53bc3289d30ad41faab58e4dd
dphoenix <dphoenix@users.noreply.github.com>
2025-08-08 15:58:19 -0400

    Update mismatched_return_type_example.py
    Added type for list

--- type-hint-arrow-samples/mismatched_return_type_example.py
+++ type-hint-arrow-samples/mismatched_return_type_example.py
@@ -1,6 +1,7 @@
-def get_number_of_titles(titles: list) -> str:
+def get_number_of_titles(titles: list[str]) -> str:
     return len(titles)
 
 
 games = ["Dragon Quest", "Final Fantasy", "Age of Empires"]
 print(f"Number of titles: {get_number_of_titles(games)}")
+
