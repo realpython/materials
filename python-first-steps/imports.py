4e4de928bd76b6d2d977ec668c1f2d773b85a1b2
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-09-08 13:09:04 +0200

    Fix linter issues

--- python-first-steps/imports.py
+++ python-first-steps/imports.py
@@ -1,11 +1,9 @@
 import math  # Module import
+from math import pi as PI  # Import with alias
+from math import sqrt  # Function import
 
 print(math.sqrt(16))
 
-from math import sqrt  # Function import
-
 print(sqrt(25))
-
-from math import pi as PI  # Import with alias
 
 print(PI)
