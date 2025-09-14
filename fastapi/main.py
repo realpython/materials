92bd79240e16221013a6a183c9df0fabd108bd7a
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-09-14 15:38:02 +0200

    Post final-QA

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 from fastapi import FastAPI
@@ -24,7 +22,7 @@ class Book(BaseModel):
 
 
 @app.get("/books")
-def get_books(limit: Optional[int] = None):
+def get_books(limit: int | None = None):
     """Get all books, optionally limited by count."""
     if limit:
         return {"books": books[:limit]}
