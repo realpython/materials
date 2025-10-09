4b3b471c036c9c951b3ba88f2296046656279f14
Philipp <filip.axani+github@gmail.com>
2025-10-09 14:38:06 +0200

    Update code from review and fix requirements.txt file

--- fastapi-jinja2-template/main.py
+++ fastapi-jinja2-template/main.py
@@ -12,30 +12,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-def generate_color():
-    return f"#{''.join(random.choices(hexdigits.lower(), k=6))}"
-
-
 @app.get("/", response_class=HTMLResponse)
-def home():
-    html = """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Home</title>
-    </head>
-    <body>
-        <h1>Welcome to FastAPI!</h1>
-    </body>
-    </html>
-    """
-    return html
-
-
-@app.get("/random-color", response_class=HTMLResponse)
-def random_color(request: Request):
-    color = generate_color()
+def home(request: Request):
+    hex_chars = "".join(random.choices(hexdigits.lower(), k=6))
+    hex_color = f"#{hex_chars}"
+    context = {
+        "color": hex_color,
+    }
     return templates.TemplateResponse(
-        request=request, name="color.html", context={"color": color}
+        request=request, name="color.html", context=context
     )
