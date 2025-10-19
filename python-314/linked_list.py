22c8cd60cdffba002bad07f53d7a40a129bce9be
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2025-10-19 13:29:01 +0200

    Uncomment code for Python 3.14

--- python-314/linked_list.py
+++ python-314/linked_list.py
@@ -1,15 +1,13 @@
-# Uncomment for Python 3.14
-#
-# from dataclasses import dataclass
-# from typing import Any, Optional
-#
-#
-# @dataclass
-# class LinkedList:
-#     head: Node
-#
-#
-# @dataclass
-# class Node:
-#     value: Any
-#     next: Optional[Node] = None
+from dataclasses import dataclass
+from typing import Any, Optional
+
+
+@dataclass
+class LinkedList:
+    head: Node
+
+
+@dataclass
+class Node:
+    value: Any
+    next: Optional[Node] = None
