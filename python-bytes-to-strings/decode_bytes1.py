0be976e384c4e048bf5165ed261255dc9fb12bb7
Philipp <filip.axani+github@gmail.com>
2025-10-29 16:52:33 +0100

    Fix indentation

--- python-bytes-to-strings/decode_bytes1.py
+++ python-bytes-to-strings/decode_bytes1.py
@@ -3,6 +3,6 @@ from urllib.request import urlopen
 url = "https://example.com/"
 
 with urlopen(url) as response:
-     raw_bytes: bytes = response.read()
+    raw_bytes: bytes = response.read()
 
 print(f"Bytes: {raw_bytes[:100]}\n")
--- python-bytes-to-strings/decode_bytes2.py
+++ python-bytes-to-strings/decode_bytes2.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 url = "https://example.com/"
 
 with urlopen(url) as response:
-        raw_bytes: bytes = response.read()
+    raw_bytes: bytes = response.read()
 
 print(f"Bytes: {raw_bytes[:100]}\n")
 
