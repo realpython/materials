8cb4d2802d410847c05283ee82fd7451ba149485
eyrei123 <eyrei@aol.com>
2025-11-06 20:18:30 +0000

    line length

--- narwhals-python/universal_processing.py
+++ narwhals-python/universal_processing.py
@@ -14,7 +14,11 @@ def universal_groupby_v1(df: IntoFrameT) -> IntoFrameT:
 
 @nw.narwhalify
 def universal_groupby_v2(df: FrameT) -> FrameT:
-    return df.group_by("party_name").agg(nw.col("last_name").count()).sort("party_name")
+    return (
+        df.group_by("party_name")
+        .agg(nw.col("last_name").count())
+        .sort("party_name")
+    )
 
 
 def universal_groupby_v3(df: IntoFrameT) -> IntoFrameT:
