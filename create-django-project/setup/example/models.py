9cbc3c435fadb1d1769505c4624825ef2239edb0
martin-martin <breuss.martin@gmail.com>
2025-11-24 10:39:13 +0100

    Add noqa to scaffolding for CI

--- create-django-project/setup/example/models.py
+++ create-django-project/setup/example/models.py
@@ -1,3 +1,3 @@
-from django.db import models
+from django.db import models  # noqa: F401
 
 # Create your models here.
--- create-django-project/setup/example/tests.py
+++ create-django-project/setup/example/tests.py
@@ -1,3 +1,3 @@
-from django.test import TestCase
+from django.test import TestCase  # noqa: F401
 
 # Create your tests here.
