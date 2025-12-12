af7524f37bffcfdc1ed6f41b16c15928b139ceb7
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-12-12 13:08:32 +0100

    Sample code for the article on best practices

---
+++ python-best-practices/variables_constants.py
@@ -0,0 +1,17 @@
+# Avoid this:
+# hours = 35
+
+
+# def compute_net_salary(hours):
+#     return hours * 35 * (1 - (0.04 + 0.1))
+
+
+# Favor this:
+hours = 35
+HOURLY_SALARY = 35
+SOCIAL_SECURITY_TAX_RATE = 0.04
+FEDERAL_TAX_RATE = 0.10
+
+
+def compute_net_salary(hours):
+    return hours * HOURLY_SALARY * (1 - (SOCIAL_SECURITY_TAX_RATE + FEDERAL_TAX_RATE))
