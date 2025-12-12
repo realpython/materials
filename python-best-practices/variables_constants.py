156f47d6098f5d8c8bc2b46509299f52c7b46106
Leodanis Pozo Ramos <lpozor78@gmail.com>
2025-12-12 13:14:55 +0100

    More fixes

--- python-best-practices/variables_constants.py
+++ python-best-practices/variables_constants.py
@@ -7,13 +7,12 @@
 
 
 # Favor this:
-HOURLY_SALARY = 35
-SOCIAL_SECURITY_TAX_RATE = 0.04
-FEDERAL_TAX_RATE = 0.10
+# HOURLY_SALARY = 35
+# SOCIAL_SECURITY_TAX_RATE = 0.04
+# FEDERAL_TAX_RATE = 0.10
 
-hours = 35
+# hours = 35
 
 
-def compute_net_salary(hours):
-    return hours * HOURLY_SALARY * \
-        (1 - (SOCIAL_SECURITY_TAX_RATE + FEDERAL_TAX_RATE))
+# def compute_net_salary(hours):
+    # return hours * HOURLY_SALARY * (1 - (SOCIAL_SECURITY_TAX_RATE + FEDERAL_TAX_RATE))
