09eb38269e9b917b9e291fc7957b5912965b6272
eyrei123 <eyrei@aol.com>
2026-02-01 16:13:20 +0000

    Post TR2 Attempt 3

--- tinydb/update_db_v3.py
+++ tinydb/update_db_v3.py
@@ -2,4 +2,6 @@ from tinydb import TinyDB
 
 with TinyDB("ten_countries.json") as countries_db:
     countries_table = countries_db.table(name="countries")
-    countries_table.update({"source": "Official estimate"}, doc_ids=[7, 9])
+    countries_table.update(
+    {"source": "Official estimate"}, doc_ids=[7, 9]
+    )
