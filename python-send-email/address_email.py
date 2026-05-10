7b9fa039fea4f533c57dedf5c5abd67403814a24
Jon Fincher <jfincher42@gmail.com>
2026-05-10 01:49:40 -0500

    Support code checkin for "Sending Emails with Python" article (#752)
    * Support code checkin
    
    * Updating to reflect Ruby linter changes
    
    * Updating with review feedback, renaming to snake case
    
    * Updating with review feedback, adding two other samples
    
    * Final QA
    
    ---------
    
    Co-authored-by: Bartosz Zaczyński <bartosz.zaczynski@gmail.com>

---
+++ python-send-email/address_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import csv
+from email.message import EmailMessage
+from email.headerregistry import Address
+
+from send_msg import send
+
+sender_email = "my@gmail.com"
+sender = Address(display_name="Me", addr_spec=sender_email)
+
+with open("contacts.csv") as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip header row
+    for name, email, grade in reader:
+        recipient = Address(display_name=name, addr_spec=email)
+        msg = EmailMessage()
+        msg["to"] = recipient
+        msg["from"] = sender
+        msg["Subject"] = "Your grade"
+        msg.set_content(f"Congratulations, {name}, you got a {grade}.")
+
+        send(msg, sender_email)
