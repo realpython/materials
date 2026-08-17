c562c53d5562676103134d7c1dd102f368989fae
mostafaibrahim17 <mostafa.ibrahim.17@ucl.ac.uk>
2026-08-17 15:58:30 +0300

    seperated files into slow and fast

---
+++ python-performance-optimization-guide/step-3-fast.py
@@ -0,0 +1,15 @@
+import timeit
+
+words = ["apple", "pear", "apple", "cherry", "pear", "apple"] * 1000
+
+from collections import Counter
+
+def counter_count(items):
+    return Counter(items)
+
+print(counter_count(words))
+
+counter_time = timeit.timeit(
+    lambda: counter_count(words), number=1000
+)
+print(f"collections.Counter: {counter_time:.4f} seconds")
