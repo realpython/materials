a6125f9fc75d84dba814f5dce8061543f6fb26b9
Shanzia Shabnom Mithun <shabnommithun@gmail.com>
2026-08-19 13:59:43 +0600

    Fix code formatting with ruff

--- nuitka-standalone-executable/wordcount.py
+++ nuitka-standalone-executable/wordcount.py
@@ -2,7 +2,9 @@ from argparse import ArgumentParser
 from collections import Counter
 from pathlib import Path
 
-parser = ArgumentParser(description="Show the most common words in a text file.")
+parser = ArgumentParser(
+    description="Show the most common words in a text file."
+)
 parser.add_argument("file", type=Path)
 parser.add_argument("-n", "--top", type=int, default=5)
 args = parser.parse_args()
